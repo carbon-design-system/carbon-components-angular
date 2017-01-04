7b052566f34b8263bac6f908aac4d28f8591deab
Callum Smith <callums@ca.ibm.com>
2017-01-04 17:33:03 -0500

    added travis

--- demo/main.ts
+++ demo/main.ts
@@ -1,7 +1,5 @@
 import "./polyfills.ts";
 
-import "./bootstrap/x1-ui-bootstrap.min.css";
-
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./demo_app";
 
---
+++ index.ts
@@ -0,0 +1,1 @@
+export * from "src/table/table.module";
