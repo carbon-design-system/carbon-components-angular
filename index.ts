d0df4530a19a84d1be29a663876fca0662ce2166
Callum Smith <callums@ca.ibm.com>
2017-01-10 12:01:40 -0500

    fixed index.ts export paths

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export * from "src/table/table.module";
-export * from "src/glyphicon/glyphicon.module";
-export * from "src/popover/popover.module";
+export * from "./src/table/table.module";
+export * from "./src/glyphicon/glyphicon.module";
+export * from "./src/popover/popover.module";
