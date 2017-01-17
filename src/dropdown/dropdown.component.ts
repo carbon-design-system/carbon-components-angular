11a210d486d0a7bba11c263fdd38f2fbc8293f12
Callum Smith <callums@ca.ibm.com>
2017-01-17 14:45:51 -0500

    added an view baseclass and refactored dropdown and list-view to use it

--- src/dropdown/dropdown.component.ts
+++ src/dropdown/dropdown.component.ts
@@ -1,53 +1,69 @@
-import { Component, OnDestroy, Input, Output, ElementRef, Renderer, ViewEncapsulation, ContentChild } from "@angular/core";
+import {
+	Component,
+	OnDestroy,
+	Input,
+	ElementRef,
+	Renderer,
+	ViewEncapsulation,
+	ContentChild,
+	ViewChild,
+	AfterContentInit,
+	HostListener
+} from "@angular/core";
 
-import { ListView } from "./../list-view/list-view.component";
+import { View } from "./../common/view.class";
+import { KeyCodes } from "./../constant/keys";
 
 @Component({
 	selector: "cdl-dropdown",
 	template: `
-		<button class="dropdown-value {{size}}" (click)="openMenu()" [disabled]="disabled">
+		<button 
+			#dropdownHost
+			class="dropdown-value {{size}}" 
+			(click)="openMenu()" 
+			[disabled]="disabled">
 			{{displayValue}}
 			<span [class.divider]="isSplit"></span>
 			<span class="dropdown-icon" [class.open]="!menuIsClose">
-				<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-					<path class="st0" d="M14.6 4L8 10.6 1.4 4l-.8.8L8 12.3l7.4-7.5z"/>
+				<svg
+					xmlns="http://www.w3.org/2000/svg" 
+					width="16" 
+					height="16" 
+					viewBox="0 0 16 16">
+					<path d="M14.6 4L8 10.6 1.4 4l-.8.8L8 12.3l7.4-7.5z"/>
 				</svg>
 			</span>
 		</button>
-		<div class="dropdown-menu {{size}} {{type}}" [class.open]="!menuIsClose">
+		<div 
+			class="dropdown-menu {{size}} {{type}}" 
+			[class.open]="!menuIsClose">
 			<ng-content></ng-content>
 		</div>
 	`,
 	encapsulation: ViewEncapsulation.None,
 	styleUrls: ["./dropdown.component.scss"],
-	host: {"class": "dropdown-wrapper"}
+	host: {"class": "dropdown-wrapper"},
 })
-export class Dropdown implements OnDestroy {
+export class Dropdown implements OnDestroy, AfterContentInit {
 	private text: string;
 	private clickInsideComp = false;
 	private listenFn: Function;
 	private globalListenFn: Function;
-	private menuIsClose: boolean;
+	private menuIsClose: boolean = true;
 	private prevSelectItem: any;
 
-	@Input() displayValue: string;
-	@Input() size: "sm" | "default" | "lg";
-	@Input() disabled: boolean;
+	@Input() displayValue: string = "";
+	@Input() size: "sm" | "default" | "lg" = "default";
+	@Input() disabled: boolean = false;
 	@Input() selectedVal: any;
-	@Input() isSplit: boolean;
-	@Input() closeOnSelect: boolean;
-	@Input() multiSelect: boolean;
+	@Input() isSplit: boolean = false;
+	@Input() closeOnSelect: boolean = true;
+	@Input() multiSelect: boolean = false;
 
-	@ContentChild(ListView) list: ListView;
+	@ContentChild(View) view;
+	@ViewChild("dropdownHost") rootButton;
 
 	constructor(private _elementRef: ElementRef, _renderer: Renderer) {
-		this.size = "default";
-		this.menuIsClose = true;
-		this.disabled = false;
-		this.isSplit = false;
-		this.closeOnSelect = true;
-		this.multiSelect = false;
-
 		// Check for click event outside of the component
 		this.listenFn = _renderer.listen(_elementRef.nativeElement, "click", (event) => {
 			this.clickInsideComp = true;
@@ -63,17 +79,26 @@ export class Dropdown implements OnDestroy {
 		// End check for click event outside of the component
 	}
 
-	ngAfterViewInit() {
-		this.list.select.subscribe((evt) => {
+	@HostListener("keydown", ["$event"])
+	onKeyDown(ev) {
+		if (ev.which === KeyCodes.ESCAPE) {
+			this.menuIsClose = true;
+			this.rootButton.nativeElement.focus();
+		}
+	}
+
+	ngAfterContentInit() {
+		this.view.select.subscribe(ev => {
 			if (this.closeOnSelect) {
 				this.menuIsClose = true;
+				this.rootButton.nativeElement.focus();
 			}
 
-			if (!this.multiSelect && this.prevSelectItem) {
+			if (!this.multiSelect && this.prevSelectItem && ev.item !== this.prevSelectItem) {
 				this.prevSelectItem.selected = false;
 			}
 
-			this.prevSelectItem = evt.item;
+			this.prevSelectItem = ev.item;
 		});
 	}
 
