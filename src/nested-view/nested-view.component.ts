0aa38e561b56f1efe9d9a1911eaa4342ff41d2e9
Callum Smith <callums@ca.ibm.com>
2017-01-19 11:33:02 -0500

    updated nested-view to fix design bugs

--- src/nested-view/nested-view.component.ts
+++ src/nested-view/nested-view.component.ts
@@ -13,16 +13,15 @@ import { NestedViewItem } from "./nested-view-item.component";
 	selector: "cdl-nested-view",
 	template: `
 		<ul class="nested-view" [class.open]="isOpen">
-			<li>
-				<cdl-nested-view-item
-					*ngFor="let item of items"
-					[listTpl]="listTpl"
-					[listItem]="item"
-					[hasSubMenu]="!!item.subMenu"
-					[parentRef]="parent"
-					(select)="onClick($event)">
-					</cdl-nested-view-item>
-			</li>
+			<cdl-nested-view-item
+				*ngFor="let item of items"
+				[listTpl]="listTpl"
+				[listItem]="item"
+				[hasSubMenu]="!!item.subMenu"
+				[parentRef]="parent"
+				(select)="onClick($event)"
+				[indent]="indent">
+			</cdl-nested-view-item>
 		</ul>
 	`,
 	providers: [{provide: View, useExisting: forwardRef(() => NestedView)}]
@@ -32,6 +31,7 @@ export class NestedView implements View {
 	@Input() isOpen: Boolean = false;
 	@Input() parent: any = null;
 	@Input() listTpl: string | TemplateRef<any>;
+	@Input() indent: number = 1;
 
 	@Output() select: EventEmitter<Object> = new EventEmitter<Object>();
 
