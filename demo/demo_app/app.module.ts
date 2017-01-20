f193bd162a3ff183636d1a1cb349f9560562df40
Rick Gao <Rick.R.Gao@ibm.com>
2017-01-20 11:43:06 -0500

    fixed closing bracket, added dist to gitignore

--- demo/demo_app/app.module.ts
+++ demo/demo_app/app.module.ts
@@ -22,16 +22,13 @@ import { ListViewDemo } from "./list_view_demo/list-view-demo.component";
 import { ListViewModule } from "./../../src/list-view/list-view.module";
 import { DropdownModule } from "./../../src/dropdown/dropdown.module";
 import { DropdownDemo } from "./dropdown_demo/dropdown_demo.component";
-<<<<<<< HEAD
 import { TopNavDemo } from "./top_nav_demo/top-nav-demo.component";
 import { TopNavModule } from "./../../src/top-nav/top-nav.module";
-=======
 import { NestedViewDemo } from "./nested_view_demo/nested-view-demo.component";
 import { NestedViewModule } from "./../../src/nested-view/nested-view.module";
 import { TypeaheadDemo } from "./typeahead_demo/typeahead-demo.component";
 import { TypeaheadModule } from "./../../src/typeahead/typeahead.module";
 
->>>>>>> 6ecebf669cdd22672f512cc1e98813202febaf4c
 
 @NgModule({
 	declarations: [
@@ -43,14 +40,11 @@ import { TypeaheadModule } from "./../../src/typeahead/typeahead.module";
 		PopoverDemo,
 		TabsDemo,
 		ListViewDemo,
-<<<<<<< HEAD
 		DropdownDemo,
-		TopNavDemo
-=======
+		TopNavDemo,
 		NestedViewDemo,
 		DropdownDemo,
 		TypeaheadDemo
->>>>>>> 6ecebf669cdd22672f512cc1e98813202febaf4c
 	],
 	imports: [
 		BrowserModule,
@@ -63,11 +57,8 @@ import { TypeaheadModule } from "./../../src/typeahead/typeahead.module";
 		ListViewModule,
 		NestedViewModule,
 		DropdownModule,
-<<<<<<< HEAD
 		TopNavModule,
-=======
 		TypeaheadModule,
->>>>>>> 6ecebf669cdd22672f512cc1e98813202febaf4c
 		RouterModule.forRoot([
 			{
 				path: "",
@@ -106,13 +97,12 @@ import { TypeaheadModule } from "./../../src/typeahead/typeahead.module";
 				component: DropdownDemo
 			},
 			{
-<<<<<<< HEAD
 				path: "top-nav",
 				component: TopNavDemo
-=======
+			},
+			{
 				path: "typeahead",
 				component: TypeaheadDemo
->>>>>>> 6ecebf669cdd22672f512cc1e98813202febaf4c
 			}
 		])
 	],
