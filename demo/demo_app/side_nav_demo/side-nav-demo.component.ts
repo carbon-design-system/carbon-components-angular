465eb05f9d8f7c0eb47a4d5472e9873d752e2575
Rick Gao <Rick.R.Gao@ibm.com>
2017-01-23 12:08:35 -0500

    added side-nav component files, removed unnecessary list-view dependancy to top-nav

---
+++ demo/demo_app/side_nav_demo/side-nav-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+	selector: "side-nav-demo",
+	templateUrl: "./side-nav-demo.component.html"
+})
+
+export class SideNavDemo {
+
+}
