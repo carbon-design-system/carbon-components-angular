57fb4ef5805f33a69cff6dfc1f0e47879c2d09f7
Amir guirguis <guiamir@ca.ibm.com>
2017-01-31 17:28:58 -0500

    adding missed scenarios

--- demo/demo_app/alert_demo/alert-demo-custom.ts
+++ demo/demo_app/alert_demo/alert-demo-custom.ts
@@ -8,16 +8,17 @@ import {
 @Component({
 	selector: "cdl-alert-custom",
 	template: `
-	<div class="alert-banner {{alertObj.type}}">
-		{{alertObj.message}}
-		- This is a custom alert component you can pass in.
-		<a href="http://www.ibm.com/ca-en/" target="_blank">click me to visit IBM</a>
-
-		<button class="clear-button" (click)="onClose()">
+	<div class="alert alert-{{alertObj.type}}">
+		<button class="close-btn" (click)="onClose()" aria-label="Close">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
 			<path d="M14.5 2.6l-1.1-1.1L8 6.9 2.6 1.5 1.5 2.6 6.9 8l-5.4 5.4 1.1 1.1L8 9.1l5.4 5.4 1.1-1.1L9.1 8z"/>
 			</svg>
 		</button>
+		<p>{{alertObj.message}}
+		- This is a custom alert component you can pass in.
+		<a href="http://www.ibm.com/ca-en/" target="_blank">click me to visit IBM</a></p>
+
+
 	</div>
 		`
 })
