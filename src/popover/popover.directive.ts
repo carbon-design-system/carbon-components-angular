53d8c0673289274fb16f2d6afcdc1e5159724ccc
Amir guirguis <guiamir@ca.ibm.com>
2017-02-07 18:20:49 -0500

    adding focus state on tooltip

--- src/popover/popover.directive.ts
+++ src/popover/popover.directive.ts
@@ -72,6 +72,8 @@ export class PopoverDirective implements OnInit {
 
 		if (this.trigger === "mouseenter") {
 			Observable.fromEvent(this.elementRef.nativeElement, "mouseout").subscribe(() => this.popoverService.close());
+			Observable.fromEvent(this.elementRef.nativeElement, "keyup").subscribe(() => this.popoverService.open(this.popoverConfig) );
+			Observable.fromEvent(this.elementRef.nativeElement, "keydown").subscribe(() => this.popoverService.close() );
 		}
 	}
 
