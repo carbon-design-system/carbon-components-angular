5f50681c44d03c13b84db8b1461d25ed329f4b34
Amir guirguis <guiamir@ca.ibm.com>
2017-02-07 17:11:37 -0500

    adding a11y for tooltips

--- src/popover/tooltip.directive.ts
+++ src/popover/tooltip.directive.ts
@@ -10,13 +10,15 @@ import {
 
 import { PopoverDirective } from "./popover.directive";
 
+let tooltipCounter = 0;
+
 @Directive({
 	selector: "[cdlTooltip]",
 	exportAs: "cdlTooltip"
 })
 export class TooltipDirective extends PopoverDirective {
 	@Input() cdlTooltip: string | TemplateRef<any>;
-
+	tooltipID: string;
 
 	constructor(private _elementRef: ElementRef, private _injector: Injector,
 			_componentFactoryResolver: ComponentFactoryResolver, private _viewContainerRef: ViewContainerRef) {
@@ -24,9 +26,13 @@ export class TooltipDirective extends PopoverDirective {
 	}
 
 	ngOnInit() {
+		tooltipCounter++;
+
 		super.ngOnInit();
 
+		this.popoverConfig.compID = "tooltip-" + tooltipCounter;
 		this.popoverConfig.isTooltip = true;
 		this.popoverConfig.content = this.cdlTooltip;
+		this._elementRef.nativeElement.setAttribute("aria-describedby", this.popoverConfig.compID);
 	}
 }
