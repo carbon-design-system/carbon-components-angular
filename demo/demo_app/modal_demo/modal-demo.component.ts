70a78ee81adaa2bd980ab6e804544aa9b0f4348a
Steven Massola <steven.massola@ibm.com>
2017-02-28 16:12:05 -0500

    form modal

--- demo/demo_app/modal_demo/modal-demo.component.ts
+++ demo/demo_app/modal_demo/modal-demo.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { XLModalComponent } from "./extra-large.component";
 import { ErrorModalComponent } from "./error-modal.component";
 import { SampleModalComponent } from "./sample-modal.component";
+import { FormModalComponent } from "./form-modal.component";
 import { ModalService } from "../../..";
 
 @Component({
@@ -9,6 +10,8 @@ import { ModalService } from "../../..";
 	templateUrl: "./modal-demo.component.html"
 })
 export class ModalDemo {
+
+	data: Object;
 
 	constructor(private modalService: ModalService) {}
 
@@ -20,9 +23,23 @@ export class ModalDemo {
 			case "error":
 				this.modalService.create({component: ErrorModalComponent, inputs: {}});
 				break;
+			case "form":
+				this.modalService.create({
+					component: FormModalComponent,
+					inputs: {
+						modalText: "This is where modal instructions should go.",
+						fields: ["Field1", "Field2", "Field3"],
+						submitted: this.formSubmitted.bind(this)
+					}
+				});
+				break;
 			default:
 				this.modalService.create({component: SampleModalComponent, inputs: {modalText: "Hello Universe"}});
 				break;
 		}
+	}
+
+	formSubmitted(data) {
+		this.data = data.value;
 	}
 }
