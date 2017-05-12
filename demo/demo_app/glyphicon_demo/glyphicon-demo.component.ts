9d942c4660348449939293c35b9279ae9c0cdcc2
Callum Smith <callums@ca.ibm.com>
2017-05-11 22:02:41 -0400

    added way better icon demo

--- demo/demo_app/glyphicon_demo/glyphicon-demo.component.ts
+++ demo/demo_app/glyphicon_demo/glyphicon-demo.component.ts
@@ -1,19 +1,96 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { IconService } from "./../../../src/glyphicon/glyphicon.module";
 
 @Component({
 	selector: "glyphicon-demo",
 	template: `
 	<h1>Glyphicon Demo</h1>
-
-	<cdl-glyphicon icon="Alert" size="xs"></cdl-glyphicon>
-	<cdl-glyphicon icon="Alert" size="sm"></cdl-glyphicon>
-	<cdl-glyphicon icon="Alert" size="md"></cdl-glyphicon>
-	<cdl-glyphicon icon="Alert" size="lg"></cdl-glyphicon>
-	`
+	<svg width="30" height="30"><use href="#alert_30"></use></svg>
+	<cdl-glyphicon icon="alert" size="xs"></cdl-glyphicon>
+	<cdl-glyphicon icon="alert" size="sm"></cdl-glyphicon>
+	<cdl-glyphicon icon="alert" size="md"></cdl-glyphicon>
+	<cdl-glyphicon icon="alert" size="lg"></cdl-glyphicon>
+	<input type="search" (keyup)="search($event)" class="input-field" style="width: 100%; margin-left: 0;" placeholder="Filter">
+	<div *ngFor="let set of displayMeta">
+		<h2>{{ formatName(set.sprite) }}</h2>
+		<div class="set">
+			<span *ngFor="let icon of filter(set.icons)">
+				<h3>{{ icon.name }}</h3>
+				<svg
+					width="30"
+					height="30">
+					<use [attr.href]="'#'+icon.name+'_30'"></use>
+				</svg>
+			</span>
+		</div>
+	</div>
+	`,
+	styles: [`
+		.set {
+			display: grid;
+			grid-template-columns: 200px 200px 200px;
+			justify-content: center;
+			grid-gap: 10px;
+		}
+		.set span {
+			height: 200px;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-content: center;
+			align-items: center;
+			border: 4px solid grey;
+		}
+		.set h3 {
+			text-align: center;
+		}
+		.set span svg {
+			display: block;
+			margin: 0 auto;
+		}
+		.svgs {
+			display: none;
+			height: 0;
+			width: 0;
+		}
+	`],
+	encapsulation: ViewEncapsulation.None
 })
 export class GlyphiconDemo {
+	iconMeta;
+	displayMeta;
 	constructor () {
-		IconService.setIconUrl("http://csx00509.canlab.ibm.com/icons/");
+		// IconService.setIconUrl("http://csx00509.canlab.ibm.com/icons/");
+	}
+
+	formatName(name) {
+		return name.slice(0, 1).toUpperCase() + name.slice(1).replace("_", " ");
+	}
+
+	filter(icons) {
+		return icons.filter(x => x.size === 30);
+	}
+
+	search(ev) {
+		this.displayMeta = this.iconMeta.map(sprite => {
+			return Object.assign({}, sprite, {
+				icons: sprite.icons.filter(icon => icon.name.includes(ev.target.value))
+			});
+		});
+	}
+
+	async ngAfterViewInit() {
+		this.iconMeta = await fetch("http://peretz-icons.mybluemix.net/meta.json").then(res => res.json());
+		console.log(this.iconMeta);
+		let svgContain = document.createElement("div");
+		svgContain.classList.add("svgs");
+		for(let set of this.iconMeta) {
+			// fetch the svgs
+			let rawSVG = await fetch(`http://peretz-icons.mybluemix.net/${set.sprite}.svg`)
+				.then(res => res.text());
+			svgContain.innerHTML += rawSVG;
+		}
+		document.body.appendChild(svgContain);
+		this.displayMeta = this.iconMeta;
 	}
 }
--- src/glyphicon/glyphicon.component.ts
+++ src/glyphicon/glyphicon.component.ts
@@ -1,68 +1,50 @@
 import { Component, OnChanges, OnInit, Input } from "@angular/core";
-import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 import { IconService } from "./glyphicon.service";
 
 @Component({
 	selector: "cdl-glyphicon",
-	template: `<span 
-					class="cdl-glyphicon" 
-					[ngStyle]="{width: iconSize + 'px', height: iconSize + 'px'}" 
-					[innerHTML]="iconSVG">
+	template: `<span
+					class="cdl-glyphicon"
+					[ngStyle]="{width: iconSize + 'px', height: iconSize + 'px'}">
+					<svg
+						[attr.width]="iconSize"
+						[attr.height]="iconSize">
+						<use [attr.href]="'#'+icon+'_'+clampSize(iconSize)"></use>
+					</svg>
 				</span>`,
 	styleUrls: ["./glyphicon.component.css"],
 	providers: [IconService]
 })
-export class Glyphicon implements OnChanges, OnInit {
-	// deps
+export class Glyphicon implements OnChanges {
+	public iconSize: string;
 	public icons: IconService;
-	public sanitizer: DomSanitizer;
-	// props
 	@Input() icon = "";
 	@Input() size = "sm";
 
-	// internal structures etc
-	public iconSize: string;
-	public iconSVG: SafeHtml;
-	constructor(icons: IconService, sanitizer: DomSanitizer) {
+	constructor(icons: IconService) {
 		this.icons = icons;
-		this.sanitizer = sanitizer;
 	}
 
-	getIcon() {
-		if (!this.size) {
-			this.size = "sm";
-		} else if (parseFloat(this.size)) {
-			this.size = "lg";
+	clampSize(size) {
+		size = parseInt(size, 10);
+		if (size <= 14) {
+			return 14;
 		}
-		return this.icons.getIcon(this.icon, this.size).then(icon => {
-			// forcebly set the height/width of the svg
-			let tmp: HTMLElement = document.createElement("div");
-			tmp.innerHTML = icon;
-			// TypeScript doesn"t realize this is an HTMLElement,
-			//  and so complains unless we do this
-			let fc: HTMLElement = <HTMLElement>tmp.firstChild;
-			fc.setAttribute("height", this.icons.size2px(this.size));
-			fc.setAttribute("width", this.icons.size2px(this.size));
-			return this.sanitizer.bypassSecurityTrustHtml(tmp.innerHTML);
-		});
-	}
-
-	ngOnInit() {
-		this.getIcon().then(icon => {
-			this.iconSVG = icon;
-		});
+		if (size > 14 && size <= 16) {
+			return 16;
+		}
+		if (size > 16 && size <= 20) {
+			return 20;
+		}
+		if (size >= 30) {
+			return 30;
+		}
 	}
 
 	ngOnChanges(changes) {
 		// init icon and watch for changes
 		if (changes.size) {
 			this.iconSize = this.icons.size2px(changes.size.currentValue);
-		}
-
-		if (changes.icon) {
-			this.getIcon().then(icon => {
-				this.iconSVG = icon;
-			});
 		}
 	}
 }
