8b4c7f89b16a122d647a0c9b654ec821dd0dfd2c
Callum Smith <callums@ca.ibm.com>
2017-08-14 11:15:22 -0400

    document icon service

--- src/icon/sprite.component.ts
+++ src/icon/sprite.component.ts
@@ -7,6 +7,10 @@ import {
 import { Http } from "@angular/http";
 import { IconService } from "./icon.service";
 
+/**
+ * Used to load sprites and is generally used at the root of the application. 
+ * Page specific sprites may be loaded on that page, but do note that may result in unintened network requets.
+ */
 @Component({
 	selector: "n-sprite",
 	template: ``,
@@ -20,13 +24,21 @@ import { IconService } from "./icon.service";
 	providers: [IconService]
 })
 export class Sprite implements AfterViewInit {
+	/** name used to requst sprite from the baseURL */
 	@Input() sprite = "";
-	@Input() baseURL = "https://peretz-icons.mybluemix.net/";
 
+	/**
+	 * instantiate the component and instances of http and iconService
+	 * 
+	 * @param {Http} http 
+	 * @param {ElementRef} _elementRef 
+	 * @param {IconService} icons 
+	 */
 	constructor(private http: Http,
 		private _elementRef: ElementRef,
 		private icons: IconService) {}
 
+	/** load the sprite and inject it into the rendered DOM */
 	ngAfterViewInit() {
 		this.icons.getSprite(this.sprite).then(rawSVG => {
 			this._elementRef.nativeElement.innerHTML = rawSVG;
