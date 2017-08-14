59876fe9b03f6fd639195f555cbe51cbbce407c6
Callum Smith <callums@ca.ibm.com>
2017-08-14 12:23:24 -0400

    fixed sample-base

--- src/sample/sample-base.class.ts
+++ src/sample/sample-base.class.ts
@@ -5,8 +5,12 @@ import { SampleInterface } from "./sample.interface";
  * This is a sample class used to demonstrate how a base class should be defined
  */
 export class SampleBase {
+	/** specify input behaviour */
 	@Input() foo: SampleInterface;
+	/** specify output behaviour */
 	@Output() bar: EventEmitter<Object>;
+	/** required template types */
 	public type: "single" | "multi" = "single";
-	getNextFoo(): SampleInterface { return; }
+	/** required methods */
+	private getNextFoo(): SampleInterface { return; }
 }
--- src/sample/sample.interface.ts
+++ src/sample/sample.interface.ts
@@ -2,6 +2,8 @@
  * Interface to demonstate how new types should be specified
  */
 export interface SampleInterface {
+	/** this field is required */
 	required: string;
+	/** this field isnt */
 	optional?: boolean;
 }
