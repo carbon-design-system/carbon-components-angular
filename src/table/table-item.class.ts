5f94df5b89305e4e68951a1144664afcddc9ffe8
Zvonimir Fras <zvonimir.fras@ibm.com>
2017-09-01 12:34:42 -0400

    Add more table documentation

--- src/table/table-item.class.ts
+++ src/table/table-item.class.ts
@@ -5,7 +5,49 @@ import {
 
 
 export class TableItem {
+	/**
+	 * Data for the table item.
+	 *
+	 * @type {*}
+	 * @memberof TableItem
+	 */
 	data: any;
+
+	/**
+	 * Used to display data in a desired way.
+	 *
+	 * If not provided, displays data as a simple string.
+	 *
+	 * Usage:
+	 *
+	 * In a component where you're using the table create a template like:
+	 *
+	 * ```html
+	 * <ng-template #customItemTemplate let-data="data">
+	 * 		<i><a [routerLink]="data.link">{{data.name}}</a></i>
+	 * </ng-template>
+	 * ```
+	 * where we assume your data contains `link` and `name`. `let-data="data"` is
+	 * necessary for you to be able to access item's data in the template.
+	 *
+	 * Create `ViewChild` property with:
+	 *
+	 * ```typescript
+	 * @ViewChild("customItemTemplate")
+	 * private customItemTemplate: TemplateRef<any>;
+	 * ```
+	 *
+	 * Set the template to the table item, for example:
+	 *
+	 * ```typescript
+	 * this.model.data = [
+	 * 		[new TableItem({data: {name: "Custom item", link: "/table"}, template: this.customItemTemplate})]
+	 * ];
+	 * ```
+	 *
+	 * @type {TemplateRef<any>}
+	 * @memberof TableItem
+	 */
 	template: TemplateRef<any>;
 
 	constructor(rawData?: any) {
