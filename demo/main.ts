d0d4665b0bf381080dd58ed8672fcabb0bc81fee
Callum Smith <callums@ca.ibm.com>
2017-09-13 12:29:27 -0400

    included matter

--- demo/main.ts
+++ demo/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
 import { AppModule } from "./demo-app";
 
-import "!style-loader!raw-loader!sass-loader!../src/core/common.scss";
+import "!style-loader!raw-loader!sass-loader!./../node_modules/@peretz/matter/scss/matter.scss";
 
 if (window.location.hostname === "pages.github.ibm.com") {
 	enableProdMode();
