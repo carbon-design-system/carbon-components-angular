8c48053fa012f94c8f2f605d99b6fc8a0e396649
Zvonimir Fras <zvonimir.fras@ibm.com>
2017-12-21 13:18:58 -0500

    Document charts

--- src/charts/bar-chart.component.ts
+++ src/charts/bar-chart.component.ts
@@ -10,6 +10,16 @@ import { BaseAxisChart } from "./base-axis-chart.component";
 
 import { BarChart as BC } from "@peretz/charts/bundle/bundle.js";
 
+/**
+ * Wrapper around `BarChart` in peretz charts library
+ *
+ * Most functions just call their equivalent from the chart library.
+ *
+ * @export
+ * @class BarChart
+ * @extends {BaseAxisChart}
+ * @implements {AfterViewInit}
+ */
 @Component({
 	selector: "n-bar-chart",
 	template: `
@@ -21,16 +31,43 @@ import { BarChart as BC } from "@peretz/charts/bundle/bundle.js";
 	encapsulation: ViewEncapsulation.None
 })
 export class BarChart extends BaseAxisChart implements AfterViewInit {
+	/**
+	 * Data passed to charts library for displaying
+	 *
+	 * @type {*}
+	 * @memberof BarChart
+	 */
 	@Input() data: any;
+	/**
+	 * Options passed to charts library
+	 *
+	 * @type {*}
+	 * @memberof BarChart
+	 */
 	@Input() options: any;
+	/**
+	 * Chart container element ref
+	 *
+	 * @memberof BarChart
+	 */
 	@ViewChild("nChart") chartRef;
 
+	/**
+	 * Runs after view init to create a chart, attach it to `chartRef` and draw it.
+	 *
+	 * @memberof BarChart
+	 */
 	ngAfterViewInit() {
 		this.chart = new BC(this.chartRef.nativeElement, this.options, this.data);
 		Object.assign(this, this.chart);
 		this.drawChart();
 	}
 
+	/**
+	 * Calls `drawChart()` from the chart library.
+	 *
+	 * @memberof BarChart
+	 */
 	drawChart() {
 		this.chart.drawChart();
 	}
--- src/charts/double-axis-line-chart.component.ts
+++ src/charts/double-axis-line-chart.component.ts
@@ -10,6 +10,16 @@ import { BaseAxisChart } from "./base-axis-chart.component";
 
 import { DoubleAxisLineChart as DALC } from "@peretz/charts/bundle/bundle.js";
 
+/**
+ * Wrapper around `DoubleAxisLineChart` in peretz charts library
+ *
+ * Most functions just call their equivalent from the chart library.
+ *
+ * @export
+ * @class DoubleAxisLineChart
+ * @extends {BaseAxisChart}
+ * @implements {AfterViewInit}
+ */
 @Component({
 	selector: "n-double-axis-line-chart",
 	template: `
@@ -21,16 +31,43 @@ import { DoubleAxisLineChart as DALC } from "@peretz/charts/bundle/bundle.js";
 	encapsulation: ViewEncapsulation.None
 })
 export class DoubleAxisLineChart extends BaseAxisChart implements AfterViewInit {
+	/**
+	 * Data passed to charts library for displaying
+	 *
+	 * @type {*}
+	 * @memberof DoubleAxisLineChart
+	 */
 	@Input() data: any;
+	/**
+	 * Options passed to charts library
+	 *
+	 * @type {*}
+	 * @memberof DoubleAxisLineChart
+	 */
 	@Input() options: any;
+	/**
+	 * Chart container element ref
+	 *
+	 * @memberof DoubleAxisLineChart
+	 */
 	@ViewChild("nChart") chartRef;
 
+	/**
+	 * Runs after view init to create a chart, attach it to `chartRef` and draw it.
+	 *
+	 * @memberof DoubleAxisLineChart
+	 */
 	ngAfterViewInit() {
 		this.chart = new DALC(this.chartRef.nativeElement, this.options, this.data);
 		Object.assign(this, this.chart);
 		this.drawChart();
 	}
 
+	/**
+	 * Calls `drawChart()` from the chart library.
+	 *
+	 * @memberof DoubleAxisLineChart
+	 */
 	drawChart() {
 		this.chart.drawChart();
 	}
