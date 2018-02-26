6eae74849fd14f37d9482a78c88a9111889937ea
Cal Smith <callums@ca.ibm.com>
2018-02-26 17:45:06 -0500

    make placeholder service a singleton

--- src/modal/modal.module.ts
+++ src/modal/modal.module.ts
@@ -1,5 +1,10 @@
 // modules
-import { NgModule } from "@angular/core";
+import {
+	NgModule,
+	ComponentFactoryResolver,
+	Optional,
+	SkipSelf
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TranslateModule } from "@ngx-translate/core";
 import { StaticIcon, StaticIconModule } from "./../icon/static-icon.module";
@@ -7,6 +12,7 @@ import { StaticIcon, StaticIconModule } from "./../icon/static-icon.module";
 // imports
 import { ModalPlaceholderComponent } from "./modal-placeholder.component";
 import { ModalService } from "./modal.service";
+import { ModalPlaceholderService } from "./modal-placeholder.service";
 import { ModalComponent } from "./modal.component";
 import { ModalFooterComponent } from "./modal-footer.component";
 import { OverlayComponent } from "./overlay.component";
@@ -15,6 +21,18 @@ import { ModalHeaderComponent } from "./modal-header.component";
 // exports
 export { default as Modal } from "./modal.decorator";
 export { ModalService } from "./modal.service";
+
+// either provides a new instance of ModalPlaceholderService, or returns the parent
+export function MODAL_PLACEHOLDER_SERVICE_PROVIDER_FACTORY(parentService: ModalPlaceholderService) {
+	return parentService || new ModalPlaceholderService();
+}
+
+// placholder service *must* be a singleton to ensure the placeholder viewref is accsible globally
+export const MODAL_PLACEHOLDER_SERVICE_PROVIDER = {
+	provide: ModalPlaceholderService,
+	deps: [[new Optional(), new SkipSelf(), ModalPlaceholderService]],
+	useFactory: MODAL_PLACEHOLDER_SERVICE_PROVIDER_FACTORY
+};
 
 @NgModule({
 	declarations: [
@@ -36,7 +54,10 @@ export { ModalService } from "./modal.service";
 		ModalHeaderComponent
 	],
 	providers: [
-		ModalService
+		// a new modal service should be instantiated each time since in (for example) lazy loaded
+		// modules a new ComponentFactoryResolver is required to resolve any newly loaded modules
+		ModalService,
+		MODAL_PLACEHOLDER_SERVICE_PROVIDER
 	],
 	imports: [
 		CommonModule,
