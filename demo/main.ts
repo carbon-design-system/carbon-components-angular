43cae225749e8b27bca3836245361595e16bab80
Cal Smith <callums@ca.ibm.com>
2018-04-23 16:44:39 -0400

    remove duplicate matter scss import

--- demo/main.ts
+++ demo/main.ts
@@ -6,9 +6,6 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
 import { AppModule } from "./demo-app";
 
-// matter scss
-import "!style-loader!raw-loader!sass-loader!./../node_modules/@peretz/matter/scss/matter.scss";
-
 if (window.location.hostname === "pages.github.ibm.com") {
 	enableProdMode();
 }
