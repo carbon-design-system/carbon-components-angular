e09b3050e976d4cdf8838b819896e22815d64de1
Cal Smith <callums@ca.ibm.com>
2018-04-30 17:29:40 -0400

    add back typings import because reasons

--- demo/main.ts
+++ demo/main.ts
@@ -1,5 +1,8 @@
 import "./polyfills.ts";
 
+// for reasons that are not entirely clear, we need this in order to build the demo
+import "./demo-app/typings.d";
+
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
 import { AppModule } from "./demo-app";
