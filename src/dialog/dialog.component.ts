87cc8f8c5d801f9a3bde5b89e31d99e06c19b035
Cal Smith <callums@ca.ibm.com>
2018-05-01 15:14:44 -0400

    limit the work to appendToBody dialogs

--- src/dialog/dialog.component.ts
+++ src/dialog/dialog.component.ts
@@ -161,21 +161,24 @@ export class Dialog implements OnInit, AfterViewInit, OnDestroy {
 			);
 		};
 
-		// walk the parents and subscribe to all the scroll events we can
-		while (node.parentElement && node !== document.body) {
-			if (isScrollableElement(node)) {
-				observables.push(Observable.fromEvent(node, "scroll"));
+		// only do the work to find the scroll containers if we're appended to body
+		if (this.dialogConfig.appendToBody) {
+			// walk the parents and subscribe to all the scroll events we can
+			while (node.parentElement && node !== document.body) {
+				if (isScrollableElement(node)) {
+					observables.push(Observable.fromEvent(node, "scroll"));
+				}
+				node = node.parentElement;
 			}
-			node = node.parentElement;
+			// subscribe to the observable, and update the position and visibility
+			const scrollObservable = Observable.merge(...observables);
+			this.scrollSubscription = scrollObservable.subscribe((event: any) => {
+				this.placeDialog();
+				if (!isVisibleInContainer(this.dialogConfig.parentRef.nativeElement, event.target)) {
+					this.doClose();
+				}
+			});
 		}
-		// subscribe to the observable, and update the position and visibility
-		const scrollObservable = Observable.merge(...observables);
-		this.scrollSubscription = scrollObservable.subscribe((event: any) => {
-			this.placeDialog();
-			if (!isVisibleInContainer(this.dialogConfig.parentRef.nativeElement, event.target)) {
-				this.doClose();
-			}
-		});
 	}
 
 	/**
