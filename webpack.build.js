1d568598a03693c213ffa9a57b5bfe82892f0a85
Cal Smith <callums@ca.ibm.com>
2018-05-08 13:53:47 -0400

    replace awesome-typescript-loader with ts-loader

--- webpack.build.js
+++ webpack.build.js
@@ -59,7 +59,7 @@ module.exports = [{
 		rules: [
 			{
 				test: /\.ts$/,
-				loaders: ["awesome-typescript-loader", "tslint-loader"]
+				loaders: ["ts-loader", "tslint-loader"]
 			},
 			{
 				test: /\.html$/,
