89403cce88b6d56eb30ebc513733fdaf76a8ac97
Cal Smith <callums@ca.ibm.com>
2018-08-15 12:23:58 -0400

    update deploy.sh to push to gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,3 +8,19 @@ if [[ ${TRAVIS_TEST_RESULT=0} == 1 ]]; then
 fi
 
 npm run semantic-release
+
+# deploy to gh pages
+if [[ $TRAVIS_BRANCH == "master" ]]; then
+	mkdir pages
+	cp -R dist/docs/documentation pages/
+	cp -R dist/docs/storybook/* pages
+
+	git config user.name "Carbon Deploy"
+
+	cd pages
+	git init
+	git add .
+	git commit -m "Deploy to GitHub Pages"
+	git push --force "git@github.com:ibm/carbon-components-angular.git" master:gh-pages > /dev/null 2>&1
+	exit 0;
+fi
