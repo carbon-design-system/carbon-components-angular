e755e373cd80838412407de3f7a923eb651aa795
Cal Smith <calsmith@protonmail.com>
2018-08-21 11:56:03 -0400

    feat(accordion): add basic accordion component

---
+++ src/accordion/accordion-item.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, HostBinding, Host, Output, EventEmitter } from "@angular/core";
+
+@Component({
+	selector: "ibm-accordion-item",
+	template: `
+		<button
+			[attr.aria-expanded]="expanded"
+			[attr.aria-controls]="id"
+			(click)="toggleExpanded()"
+			class="bx--accordion__heading">
+			<svg
+				class="bx--accordion__arrow"
+				width="7"
+				height="12"
+				viewBox="0 0 7 12">
+          		<path fill-rule="nonzero" d="M5.569 5.994L0 .726.687 0l6.336 5.994-6.335 6.002L0 11.27z"/>
+			</svg>
+			 <p class="bx--accordion__title">{{title}}</p>
+		</button>
+		<div [id]="id" class="bx--accordion__content">
+			<ng-content></ng-content>
+		</div>
+	`
+})
+export class AccordionItem {
+	static accodionItemCount = 0;
+	@Input() title = `Title ${AccordionItem.accodionItemCount}`;
+	@HostBinding("class.bx--accordion__item--active") @Input() expanded = false;
+	@Input() id = `accordion-item-${AccordionItem.accodionItemCount}`;
+	@Output() selected = new EventEmitter();
+
+	@HostBinding("class") itemClass = "bx--accordion__item";
+	@HostBinding("style.display") @HostBinding("attr.role") itemType = "list-item";
+
+	constructor() {
+		AccordionItem.accodionItemCount++;
+	}
+
+	public toggleExpanded() {
+		this.expanded = !this.expanded;
+		this.selected.emit(this.id);
+	}
+}
---
+++ src/accordion/accordion.stories.ts
@@ -0,0 +1,55 @@
+import { storiesOf, moduleMetadata } from "@storybook/angular";
+import { withNotes } from "@storybook/addon-notes";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, boolean, object } from "@storybook/addon-knobs/angular";
+
+import { AccordionModule } from "../";
+
+storiesOf("Accordion", module)
+	.addDecorator(
+		moduleMetadata({
+			imports: [
+				AccordionModule
+			]
+		})
+	)
+	.addDecorator(withKnobs)
+	.add("Basic", () => ({
+		template: `
+			<ibm-accordion>
+				<ibm-accordion-item title="Section 1 title" (selected)="selected($event)">Lorem ipsum dolor sit amet, \
+				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.</ibm-accordion-item>
+				<ibm-accordion-item title="Section 2 title" (selected)="selected($event)">Lorem ipsum dolor sit amet, \
+				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.</ibm-accordion-item>
+				<ibm-accordion-item title="Section 3 title" (selected)="selected($event)">Lorem ipsum dolor sit amet, \
+				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.</ibm-accordion-item>
+				<ibm-accordion-item title="Section 4 title" (selected)="selected($event)">Lorem ipsum dolor sit amet, \
+				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.</ibm-accordion-item>
+			</ibm-accordion>
+		`,
+		props: {
+			items: [
+				{
+					content: "one"
+				},
+				{
+					content: "two"
+				},
+				{
+					content: "three"
+				},
+				{
+					content: "four"
+				}
+			],
+			selected: action("item expanded")
+		}
+	}));
