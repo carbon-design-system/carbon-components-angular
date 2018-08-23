ab51f97f59e62fdf07a92b69408a8219deac222d
Cal Smith <calsmith@protonmail.com>
2018-08-23 12:59:22 -0400

    feat(input): add basic input directive

---
+++ src/input/input.directive.ts
@@ -0,0 +1,8 @@
+import { Directive, Input, HostBinding } from "@angular/core";
+
+@Directive({
+	selector: "[ibmText]"
+})
+export class TextInput {
+	@HostBinding("class") inputClass = "bx--text-input";
+}
