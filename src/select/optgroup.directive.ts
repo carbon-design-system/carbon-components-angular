15f0fe810412a1a2263dc98b4aa2c60d299be028
Cal Smith <calsmith@protonmail.com>
2018-08-23 13:23:48 -0400

    ignore tslint for select option/optgroup directives

--- src/select/optgroup.directive.ts
+++ src/select/optgroup.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, HostBinding } from "@angular/core";
 
 @Directive({
+	// tslint:disable-next-line
 	selector: "optgroup"
 })
 export class OptGroup {
--- src/select/option.directive.ts
+++ src/select/option.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, HostBinding } from "@angular/core";
 
 @Directive({
+	// tslint:disable-next-line
 	selector: "option"
 })
 export class Option {
