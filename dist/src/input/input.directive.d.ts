39cc1fd9428fbbc3509e162c029a7551ac0fcf33
carbon-bot (from Travis CI) <carbon@us.ibm.com>
2018-08-29 17:35:57 +0000

    Deploy IBM/carbon-components-angular to github.com/IBM/carbon-components-angular.git:gh-pages

--- dist/src/input/input.directive.d.ts
+++ dist/src/input/input.directive.d.ts
@@ -1,3 +1,14 @@
+/**
+ * A directive for applying styling to an input element.
+ *
+ * Example:
+ *
+ * ```hmtl
+ * <input ibmText/>
+ * ```
+ *
+ * See the [vanilla carbon docs](http://www.carbondesignsystem.com/components/text-input/code) for more detail.
+ */
 export declare class TextInput {
     inputClass: string;
 }
