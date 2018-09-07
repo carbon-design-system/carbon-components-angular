51fad7514bb9bbdab5b2acf8d046a50578021fc7
Cal Smith <calsmith@protonmail.com>
2018-09-07 09:25:59 -0400

    chore: update index.stories with docs link

--- src/index.stories.ts
+++ src/index.stories.ts
@@ -6,6 +6,7 @@ storiesOf("Welcome", module).add("to Carbon Angular", () => ({
 	template: `
 		<h1>Carbon Components Angular</h1>
 		<h2>An Angular implementation of the Carbon Design System</h2>
+		<a href="https://angular.carbondesignsystem.com/documentation">Static documentation</a>
 	`,
 	props: {},
 }));
