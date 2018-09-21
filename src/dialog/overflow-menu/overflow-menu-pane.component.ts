8cb923823a87792a2d3354ffbb849c060ec8fb2e
Yousef Ouda <yousef@yousefs-mbp.torolab.ibm.com>
2018-09-21 10:44:28 -0400

    Update listItems

--- src/dialog/overflow-menu/overflow-menu-pane.component.ts
+++ src/dialog/overflow-menu/overflow-menu-pane.component.ts
@@ -81,6 +81,6 @@ export class OverflowMenuPane extends Dialog {
 	private listItems() {
 		const buttonClass = ".bx--overflow-menu-options__btn";
 		const disabledClass = ".bx--overflow-menu-options__option--disabled";
-		return Array.prototype.slice.call(this.dialog.nativeElement.querySelectorAll(`${buttonClass}:not(${disabledClass})`));
+		return Array.from<any>(this.dialog.nativeElement.querySelectorAll(`${buttonClass}:not(${disabledClass})`));
 	}
 }
