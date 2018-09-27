8882c43fba3d6a97687beb12b88782de77b87691
Stanislav Georgiev <sgeorgiev@ca.ibm.com>
2018-09-27 15:13:52 -0400

    Added a sortable or sortingEnabled property to allow sorting for certain columns only, for ex. column 1, 2 and 3 can be sorted but column 4 ("Actions") cannot be sorted thus should not show the sorting arrow at all. Added a class property. Added a generic property for user defined data to attach, for ex. additionalData property. Added a generic public properties initialization through the constructor (sorted, ascending, descending are unavailable at the moment). Doing that would allow to set an initial default sorting direction.

--- src/table/table.stories.ts
+++ src/table/table.stories.ts
@@ -3,7 +3,9 @@ import {
 	Component,
 	ViewChild,
 	OnInit,
-	Input
+	Input,
+	OnChanges,
+	SimpleChanges
 } from "@angular/core";
 import { storiesOf, moduleMetadata } from "@storybook/angular";
 import {
@@ -24,6 +26,50 @@ import {
 } from "../";
 
 import { clone } from "../utils/utils";
+
+
+@Component({
+	selector: "app-table",
+	template: `
+		<ibm-table
+			[model]="model"
+			[size]="size"
+			[showSelectionColumn]="showSelectionColumn"
+			[striped]="striped"
+			(sort)="simpleSort($event)">
+		</ibm-table>
+	`
+})
+class TableStory implements OnInit, OnChanges {
+	@Input() model = new TableModel();
+	@Input() size = "md";
+	@Input() showSelectionColumn = true;
+	@Input() striped = true;
+	@Input() sortable = true;
+	@Input() simpleSort;
+
+	ngOnInit() {
+		this.model.header = [
+			new TableHeaderItem({
+				data: "Name",
+				sortable: this.sortable
+			}),
+			new TableHeaderItem({
+				data: "hwer",
+				style: {"width": "auto"},
+				sortable: this.sortable
+			})
+		];
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes.sortable) {
+			for (let column of this.model.header) {
+				column.sortable = changes.sortable.currentValue;
+			}
+		}
+	}
+}
 
 @Component({
 	selector: "app-custom-table",
@@ -270,6 +316,7 @@ storiesOf("Table", module).addDecorator(
 				TranslateModule.forRoot()
 			],
 			declarations: [
+				TableStory,
 				DynamicTableStory,
 				ExpansionTableStory,
 				OverflowTableStory
@@ -279,20 +326,22 @@ storiesOf("Table", module).addDecorator(
 	.addDecorator(withKnobs)
 	.add("default", () => ({
 		template: `
-		<ibm-table
+		<app-table
 			[model]="model"
 			[size]="size"
 			[showSelectionColumn]="showSelectionColumn"
 			[striped]="striped"
-			(sort)="simpleSort($event)">
-		</ibm-table>
+			[sortable]="sortable"
+			[simpleSort]="simpleSort">
+		</app-table>
 	`,
 		props: {
 			model: simpleModel,
 			simpleSort: simpleSort,
 			size: selectV2("size", {Small: "sm", Normal: "md", Large: "lg"}, "md", "table-size-selection"),
 			showSelectionColumn: boolean("showSelectionColumn", true),
-			striped: boolean("striped", true)
+			striped: boolean("striped", true),
+			sortable: boolean("sortable", true)
 		}
 	}))
 	.add("with expansion", () => ({
