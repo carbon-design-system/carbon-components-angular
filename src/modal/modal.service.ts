ff3e16dbc77575682fc1ab989b2cb36c744b96e6
Cal Smith <calsmith@protonmail.com>
2018-10-01 16:00:47 -0400

    remove comments

--- src/modal/modal.service.ts
+++ src/modal/modal.service.ts
@@ -48,8 +48,6 @@ export class ModalService {
 		data = Object.assign({}, defaults, data);
 
 		const inputProviders = Object.keys(data.inputs).map(inputName => ({provide: inputName, useValue: data.inputs[inputName]}));
-		// const resolvedInputs = ReflectiveInjector.resolve(inputProviders);
-		// const injector = ReflectiveInjector.fromResolvedProviders(resolvedInputs, this.placeholder.viewContainerRef.parentInjector);
 		const injector = Injector.create(inputProviders);
 		const factory = this.resolver.resolveComponentFactory(data.component);
 		let focusedElement = document.activeElement;
