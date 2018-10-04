5c88b4497a75cdc61c0b8aaa96d726a42ed617bb
carbon-bot (from Travis CI) <carbon@us.ibm.com>
2018-10-04 09:28:21 +0000

    Deploy IBM/carbon-components-angular to github.com/IBM/carbon-components-angular.git:gh-pages

--- dist/src/dropdown/dropdown.component.d.ts
+++ dist/src/dropdown/dropdown.component.d.ts
@@ -147,7 +147,7 @@ export declare class Dropdown implements OnInit, AfterContentInit, OnDestroy {
      * @returns {string}
      * @memberof Dropdown
      */
-    buildClass(): "dropdown--sm" | "dropdown" | "dropdown--lg";
+    buildClass(): "dropdown" | "dropdown--sm" | "dropdown--lg";
     /**
      * Propagates the injected `value`.
      * @param {*} value
