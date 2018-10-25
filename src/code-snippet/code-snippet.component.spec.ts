b984ea73934102077d942ba12a351d1a1f43825b
Zvonimir Fras <zvonimir@zvonimirfras.com>
2018-10-25 16:04:56 -0400

    Fix test

--- src/code-snippet/code-snippet.component.spec.ts
+++ src/code-snippet/code-snippet.component.spec.ts
@@ -1,6 +1,7 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { StaticIconModule } from "../icon/static-icon.module";
+import { I18nModule } from "../i18n/i18n.module";
 
 import { CodeSnippet } from "./code-snippet.component";
 
@@ -11,8 +12,7 @@ describe("CodeSnippet", () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			declarations: [CodeSnippet],
-			imports: [BrowserAnimationsModule, StaticIconModule],
-			providers: []
+			imports: [BrowserAnimationsModule, StaticIconModule, I18nModule]
 		});
 
 		fixture = TestBed.createComponent(CodeSnippet);
