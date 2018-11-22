b944e47611c746d857728d6401672fc0ecbdd559
carbon-bot (from Travis CI) <carbon@us.ibm.com>
2018-11-22 18:05:13 +0000

    Deploy IBM/carbon-components-angular to github.com/IBM/carbon-components-angular.git:gh-pages

--- 0.0.0/dist/src/notification/notification-display.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationRef } from "@angular/core";
-export declare class NotificationDisplayService {
-    protected applicationRef: ApplicationRef;
-    constructor(applicationRef: ApplicationRef);
-    /**
-     * Programatically closes notification based on `notificationRef`.	 *
-     */
-    close(notificationRef: any): void;
-}
