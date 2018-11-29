700be0925b5ee76ff8479ba95887940581655483
Cal Smith <calsmith@protonmail.com>
2018-11-28 23:38:08 -0500

    chore(i18n): add better docs

--- src/i18n/i18n.service.ts
+++ src/i18n/i18n.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from "@angular/core";
-import { Subject, BehaviorSubject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 import { map } from "rxjs/operators";
-import { stringify } from "querystring";
 
 const EN = require("./en.json");
 
+/**
+ * Takes the Observable returned from `i18n.get` and an object of variables to replace.
+ *
+ * The keys specify the variable name in the string.
+ *
+ * Example:
+ * ```
+ * service.set({ "TEST": "{{foo}} {{bar}}" });
+ *
+ * service.replace(service.get("TEST"), { foo: "test", bar: "asdf" })
+ * ```
+ *
+ * Produces: `"test asdf"`
+ *
+ * @param subject the translation to replace variables on
+ * @param variables object of variables to replace
+ */
 export const replace = (subject, variables) => subject.pipe(
 	map<string, void>(str => {
 		const keys = Object.keys(variables);
@@ -18,12 +34,24 @@ export const replace = (subject, variables) => subject.pipe(
 	})
 );
 
+/**
+ * The I18n service is a minimal internal service used to supply our components with translated strings.
+ *
+ * All the components that support I18n also support directly passed strings.
+ * Usage of I18n is optional, and it is not reccommended for application use (libraries like ngx-translate are a better choice)
+ *
+ */
 @Injectable()
 export class I18n {
 	protected translationStrings = EN;
 
 	protected translations = new Map();
 
+	/**
+	 * Set/update the translations from an object. Also notifies all participating components of the update.
+	 *
+	 * @param strings an object of strings, should follow the same format as src/i18n/en.json
+	 */
 	public set(strings) {
 		this.translationStrings = Object.assign({}, EN, strings);
 		const translations = Array.from(this.translations);
@@ -32,6 +60,13 @@ export class I18n {
 		}
 	}
 
+	/**
+	 * When a path is specified returns an observable that will resolve to the translation string value.
+	 *
+	 * Returns the full translations object if path is not specified.
+	 *
+	 * @param path optional, looks like `"NOTIFICATION.CLOSE_BUTTON"`
+	 */
 	public get(path?) {
 		if (!path) {
 			return this.translationStrings;
@@ -45,10 +80,32 @@ export class I18n {
 		return translation;
 	}
 
+	/**
+	 * Takes the Observable returned from `i18n.get` and an object of variables to replace.
+	 *
+	 * The keys specify the variable name in the string.
+	 *
+	 * Example:
+	 * ```
+	 * service.set({ "TEST": "{{foo}} {{bar}}" });
+	 *
+	 * service.replace(service.get("TEST"), { foo: "test", bar: "asdf" })
+	 * ```
+	 *
+	 * Produces: `"test asdf"`
+	 *
+	 * @param subject the translation to replace variables on
+	 * @param variables object of variables to replace
+	 */
 	public replace(subject, variables) {
 		return replace(subject, variables);
 	}
 
+	/**
+	 * Trys to resolve a value from the provided path.
+	 *
+	 * @param path looks like `"NOTIFICATION.CLOSE_BUTTON"`
+	 */
 	protected getValueFromPath(path) {
 		let value = this.translationStrings;
 		for (const segment of path.split(".")) {
