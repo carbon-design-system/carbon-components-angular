d11e16744d7e9f979cbe312b1139282b6c5d7002
Yousef Ouda <yousef@Yousefs-MacBook-Pro.local>
2018-12-20 11:48:41 -0500

    Update variable name

--- src/file-uploader/file.component.ts
+++ src/file-uploader/file.component.ts
@@ -19,8 +19,8 @@ import { FileItem } from "./file-item.interface";
 		<p class="bx--file-filename">{{fileItem.file.name}}</p>
 		<span *ngIf="fileItem.state === 'edit'" class="bx--file__state-container" (click)="remove.emit()">
 			<svg class="bx--file-close" fill-rule="evenodd" role="img" width="16" height="16" viewBox="0 0 16 16" tabindex="0"
-				[attr.aria-label]="translations.CLOSE_BUTTON" [attr.alt]="translations.CLOSE_BUTTON">
-				<title>{{translations.CLOSE_TITLE}}</title>
+				[attr.aria-label]="translations.REMOVE_BUTTON" [attr.alt]="translations.REMOVE_BUTTON">
+				<title>{{translations.REMOVE_TITLE}}</title>
 				<path d="M8 6.586L5.879 4.464 4.464 5.88 6.586 8l-2.122 2.121 1.415 1.415L8 9.414l2.121 2.122 1.415-1.415L9.414
 				8l2.122-2.121-1.415-1.415L8 6.586zM8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"></path>
 			</svg>
