f704516015014aaa55fe956d680344bf62838fbc
Yousef Ouda <yousef@yousefs-mbp.war.can.ibm.com>
2019-01-09 14:20:30 -0500

    Remove unused import

--- src/number-input/number.component.spec.ts
+++ src/number-input/number.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 
 import { Number } from "./number.component";
