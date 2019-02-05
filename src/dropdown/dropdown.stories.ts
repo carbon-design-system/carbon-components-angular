e94df57f595240faf17709e4188d8af57f3fdc0d
Cal Smith <calsmith@protonmail.com>
2019-02-05 12:52:24 -0500

    fix(dropdown): don't focus the button on load

--- src/dropdown/dropdown.stories.ts
+++ src/dropdown/dropdown.stories.ts
@@ -17,6 +17,7 @@ storiesOf("Dropdown", module)
 	.add("Basic", () => ({
 		template: `
 		<div style="width: 300px">
+			<input type="text"/>
 			<ibm-dropdown
 				[theme]="theme"
 				placeholder="Select"
