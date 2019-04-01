f35755daafaec49570ede281e31c56f3ffbf7915
Cal Smith <calsmith@protonmail.com>
2019-04-01 13:22:31 -0400

    upgrade modal

--- src/modal/modal-header.component.ts
+++ src/modal/modal-header.component.ts
@@ -30,35 +30,7 @@ import { ExperimentalService } from "./../experimental.service";
 				class="bx--modal-close"
 				[attr.aria-label]="closeLabel"
 				(click)="onClose()">
-				<!-- old icon -->
-				<svg
-					*ngIf="!isExperimental"
-					class="bx--modal-close__icon"
-					fill-rule="evenodd"
-					height="10"
-					role="img"
-					viewBox="0 0 10 10"
-					width="10"
-					[attr.aria-label]="closeLabel">
-					<title>{{closeLabel}}</title>
-					<path d="M6.32 5L10 8.68 8.68 10 5 6.32 1.32 10 0 8.68 3.68 5 0 1.32 1.32 0 5 3.68 8.68 0 10 1.32 6.32 5z"></path>
-				</svg>
-				<!-- new icon -->
-				<svg
-					*ngIf="isExperimental"
-					focusable="false"
-					preserveAspectRatio="xMidYMid meet"
-					style="will-change: transform;"
-					xmlns="http://www.w3.org/2000/svg"
-					class="bx--modal-close__icon"
-					width="16"
-					height="16"
-					viewBox="0 0 16 16"
-					aria-hidden="true">
-					<path
-						d="M12 4.7l-.7-.7L8 7.3 4.7 4l-.7.7L7.3 8 4 11.3l.7.7L8 8.7l3.3 3.3.7-.7L8.7 8z">
-					</path>
-				</svg>
+				<ibm-icon-close16 class="bx--modal-close__icon"></ibm-icon-close16>
 			</button>
 		</header>
 
