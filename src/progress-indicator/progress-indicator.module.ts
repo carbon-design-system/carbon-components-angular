2f41f54b063936fc3fb2c7696be0f9e91d8c0dce
Cal Smith <calsmith@protonmail.com>
2019-04-01 14:34:38 -0400

    update progress indicator

--- src/progress-indicator/progress-indicator.module.ts
+++ src/progress-indicator/progress-indicator.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { CheckmarkOutline16Module } from "@carbon/icons-angular/lib/checkmark--outline/16";
+import { ErrorOutline16Module } from "@carbon/icons-angular/lib/error--outline/16";
 
 import { ProgressIndicator } from "./progress-indicator.component";
 import { ExperimentalModule } from "./../experimental.module";
@@ -12,6 +14,11 @@ import { ExperimentalModule } from "./../experimental.module";
 	exports: [
 		ProgressIndicator
 	],
-	imports: [CommonModule, ExperimentalModule]
+	imports: [
+		CommonModule,
+		ExperimentalModule,
+		CheckmarkOutline16Module,
+		ErrorOutline16Module
+	]
 })
 export class ProgressIndicatorModule { }
