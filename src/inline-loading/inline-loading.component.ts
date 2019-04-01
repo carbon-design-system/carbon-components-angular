a4d4890423acf6b380aa7d5400dc3334c5a1718a
Cal Smith <calsmith@protonmail.com>
2019-04-01 13:22:24 -0400

    upgrade inline loading

--- src/inline-loading/inline-loading.component.ts
+++ src/inline-loading/inline-loading.component.ts
@@ -14,12 +14,15 @@ import {
 				*ngIf="success === false"
 				class="bx--loading bx--loading--small">
 				<svg class="bx--loading__svg" viewBox="-75 -75 150 150">
-					<circle cx="0" cy="0" r="37.5"></circle>
+					<circle class="bx--loading__background" cx="0" cy="0" r="30" />
+					<circle class="bx--loading__stroke" cx="0" cy="0" r="30" />
 				</svg>
 			</div>
 			<svg
 				*ngIf="success === true"
-				class="bx--inline-loading__checkmark-container bx--inline-loading__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+				class="bx--inline-loading__checkmark-container bx--inline-loading__svg"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 10 10">
 				<polyline class="bx--inline-loading__checkmark" points="0.74 3.4 3.67 6.34 9.24 0.74"></polyline>
 			</svg>
 		</div>
@@ -31,19 +34,16 @@ export class InlineLoading {
 	/**
 	 * Specify the text description for the loading state.
 	 *
-	 * @memberof InlineLoading
 	 */
 	@Input() loadingText;
 	/**
 	 * Specify the text description for the success state.
 	 *
-	 * @memberof InlineLoading
 	 */
 	@Input() successText;
 	/**
 	 * Provide a delay for the `setTimeout` for success.
 	 *
-	 * @memberof InlineLoading
 	 */
 	@Input() successDelay = 1500;
 
@@ -69,7 +69,6 @@ export class InlineLoading {
 	 * Emits event after the success state is active
 	 *
 	 * @type {EventEmitter<any>}
-	 * @memberof InlineLoading
 	 */
 	@Output() onSuccess: EventEmitter<any> = new EventEmitter();
 
@@ -78,7 +77,6 @@ export class InlineLoading {
 	/**
 	 * Set to `true` if the action is completed successfully.
 	 *
-	 * @memberof InlineLoading
 	 */
 	protected _success = false;
 }
