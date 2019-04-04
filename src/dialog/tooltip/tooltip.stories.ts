43d2e076696e12c1b492e57b982cb68b98a2e38e
Yousef Ouda <yousef@yousefs-mbp.war.can.ibm.com>
2019-04-04 17:34:07 -0400

    Update tooltip story

--- src/dialog/tooltip/tooltip.stories.ts
+++ src/dialog/tooltip/tooltip.stories.ts
@@ -1,7 +1,7 @@
 import { storiesOf, moduleMetadata } from "@storybook/angular";
 import { withNotes } from "@storybook/addon-notes";
 import { action } from "@storybook/addon-actions";
-import { withKnobs, boolean, object } from "@storybook/addon-knobs/angular";
+import { withKnobs, text, select } from "@storybook/addon-knobs/angular";
 
 import { DialogModule, PlaceholderModule } from "../../";
 
@@ -27,14 +27,13 @@ storiesOf("Tooltip", module)
 				</ng-template>
 
 				<div class="bx--tooltip__label">
-					Tooltip label
+					{{triggerText}}
 					<span
 						[ibmTooltip]="template"
 						trigger="click"
-						placement="bottom"
-						style="cursor: pointer;">
-						<div role="button" class="bx--tooltip__trigger"  style="vertical-align: middle">
-							<svg fill-rule="evenodd" height="16" role="img" viewBox="0 0 16 16" width="16" aria-label="tooltip" alt="tooltip">
+						[placement]="placement">
+						<div role="button">
+							<svg style="vertical-align: middle;" fill-rule="evenodd" height="16" role="img" viewBox="0 0 16 16" width="16" aria-label="tooltip">
 								<path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 4c.6 0 1 .4 1 1s-.4 1-1 1-1-.4-1-1
 									.4-1 1-1zm2 8H6v-1h1V8H6V7h3v4h1v1z"></path>
 							</svg>
@@ -43,7 +42,11 @@ storiesOf("Tooltip", module)
 				</div>
 				<ibm-placeholder></ibm-placeholder>
 			</div>
-		`
+		`,
+		props: {
+			placement: select("Tooltip direction", ["bottom", "top", "left", "right"], "bottom"),
+			triggerText: text("Trigger text", "Tooltip label")
+		}
 	}))
 	.add("no icon", () => ({
 		template: `
@@ -59,13 +62,17 @@ storiesOf("Tooltip", module)
 
 					<span
 						[ibmTooltip]="template"
-						trigger="hover"
-						placement="bottom">
-						Tooltip label
+						trigger="click"
+						[placement]="placement">
+						{{triggerText}}
 					</span>
 					<ibm-placeholder></ibm-placeholder>
 				</div>
-			`
+			`,
+			props: {
+				placement: select("Tooltip direction", ["bottom", "top", "left", "right"], "bottom"),
+				triggerText: text("Trigger text", "Tooltip label")
+			}
 		}))
 		.add("only icon", () => ({
 			template: `
@@ -81,8 +88,8 @@ storiesOf("Tooltip", module)
 
 						<span
 							[ibmTooltip]="template"
-							trigger="hover"
-							placement="bottom">
+							trigger="click"
+							[placement]="placement">
 							<div role="button" class="bx--tooltip__trigger">
 								<svg focusable="false" aria-label="tooltip" width="16" height="16" viewBox="0 0 16 16" role="img" style="will-change: transform;">
 									<circle cx="8" cy="3" r="1"></circle><circle cx="8" cy="8" r="1"></circle><circle cx="8" cy="13" r="1"></circle>
@@ -91,19 +98,49 @@ storiesOf("Tooltip", module)
 						</span>
 						<ibm-placeholder></ibm-placeholder>
 					</div>
-				`
+				`,
+				props: {
+					placement: select("Tooltip direction", ["bottom", "top", "left", "right"], "bottom")
+				}
 			}))
 	.add("definition", () => ({
 		template: `
-			<div>
+			<div class="bx--tooltip--definition">
 				<span
-					ibmTooltip="tooltip text"
-					type="definition"
-					trigger="click"
-					placement="bottom"
-					style="cursor: pointer;">
+					[ibmTooltip]="tooltipText"
+					[definition]="true"
+					trigger="hover"
+					[placement]="placement">
 					Definition Tooltip
 				</span>
+				<ibm-placeholder></ibm-placeholder>
 			</div>
-		`
+		`,
+		props: {
+			placement: select("Tooltip direction", ["bottom", "top"], "bottom"),
+			tooltipText: text("Tooltip content", "Brief description of the dotted, underlined word above.")
+		}
+	}))
+	.add("definition icon", () => ({
+		template: `
+			<div>
+				<span
+					ibmTooltip
+					[icon]="true"
+					[placement]="placement"
+					[attr.aria-label]="tooltipText">
+					<svg width="16" height="12" viewBox="0 0 16 12">
+						<g fill-rule="nonzero">
+							<path d="M8.05 2a2.5 2.5 0 0 1 4.9 0H16v1h-3.05a2.5 2.5 0 0 1-4.9 0H0V2h8.05zm2.45 2a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0
+							3zM3.05 9a2.5 2.5 0 0 1 4.9 0H16v1H7.95a2.5 2.5 0 0 1-4.9 0H0V9h3.05zm2.45 2a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
+						</g>
+					</svg>
+				</span>
+				<ibm-placeholder></ibm-placeholder>
+			</div>
+		`,
+		props: {
+			placement: select("Tooltip direction", ["bottom", "top"], "bottom"),
+			tooltipText: text("Tooltip content", "Filter")
+		}
 	}));
