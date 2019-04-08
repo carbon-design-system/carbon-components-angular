0ebcb9234ff0aab4ceed3745d93922cdc732fd96
Cal Smith <calsmith@protonmail.com>
2019-04-08 14:33:26 -0400

    fix(dropdown-list): selected emitting on init when nothing is selected

--- src/dropdown/list/dropdown-list.component.ts
+++ src/dropdown/list/dropdown-list.component.ts
@@ -212,10 +212,12 @@ export class DropdownList implements AbstractDropdownView, AfterViewInit, OnDest
 		this.index = this._items.findIndex(item => item.selected);
 		this.setupFocusObservable();
 		setTimeout(() => {
-			if (this.type === "single") {
-				this.select.emit({ item: this._items.find(item => item.selected) });
-			} else {
-				this.select.emit(this.getSelected() || []);
+			if (this.getSelected()) {
+				if (this.type === "single") {
+					this.select.emit({ item: this._items.find(item => item.selected) });
+				} else {
+					this.select.emit(this.getSelected() || []);
+				}
 			}
 		});
 	}
