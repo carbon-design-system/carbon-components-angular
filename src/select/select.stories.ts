4692ce0502bbf211b1565f92576b4a0554c1ba30
Zvonimir Fras <zvonimir.fras@gmail.com>
2019-04-11 15:01:26 -0400

    Update src/select/select.stories.ts
    Co-Authored-By: youda97 <ouda.yousef@gmail.com>

--- src/select/select.stories.ts
+++ src/select/select.stories.ts
@@ -1,6 +1,11 @@
 import { storiesOf, moduleMetadata } from "@storybook/angular";
 import { action } from "@storybook/addon-actions";
-import { withKnobs, select, boolean, text } from "@storybook/addon-knobs/angular";
+import {
+	withKnobs,
+	select,
+	boolean,
+	text
+} from "@storybook/addon-knobs/angular";
 
 import { SelectModule } from "../";
 
