fbd0692099a70e8508c9d776b3bd164ff073e469
Oliver Rudolph <rudolpho1@users.noreply.github.com>
2019-04-12 15:33:14 +0200

    Change import to one  line per entry

--- src/datepicker-input/datepicker-input.component.ts
+++ src/datepicker-input/datepicker-input.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+	Component,
+	Input,
+	Output,
+	EventEmitter
+} from "@angular/core";
 
 @Component({
 	selector: "ibm-date-picker-input",
