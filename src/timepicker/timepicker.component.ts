287bbb07b8c8778d40d55ccfd2a70b66ed3022e7
Maged Hennawy <maged.hennawy@ibm.com>
2019-04-16 14:22:34 -0400

    Address host class issue

--- src/timepicker/timepicker.component.ts
+++ src/timepicker/timepicker.component.ts
@@ -2,36 +2,33 @@ import {
 	Component,
 	Input,
 	Output,
-	EventEmitter
+	EventEmitter,
+	HostBinding
 } from "@angular/core";
 
 @Component({
 	selector: "ibm-timepicker",
 	template: `
-		<div class="bx--form-item">
-			<div class="bx--time-picker">
-				<div
-					[ngClass]="{
-						'bx--select--light': theme === 'light',
-						'bx--skeleton': skeleton
-					}"
-					class="bx--time-picker__input">
-					<label *ngIf="!skeleton" [attr.for]="id" class="bx--label">{{label}}</label>
-					<input
-						#timePicker
-						[value]="value"
-						[placeholder]="placeholder"
-						[pattern]="pattern"
-						[attr.id]="id"
-						[disabled]="disabled"
-						maxlength="5"
-						(change)="onChange($event)"
-						type="string"
-						class="bx--time-picker__input-field">
-				</div>
-				<ng-content></ng-content>
+			<div
+				[ngClass]="{
+					'bx--select--light': theme === 'light',
+					'bx--skeleton': skeleton
+				}"
+				class="bx--time-picker__input">
+				<label *ngIf="!skeleton" [attr.for]="id" class="bx--label">{{label}}</label>
+				<input
+					#timePicker
+					[value]="value"
+					[placeholder]="placeholder"
+					[pattern]="pattern"
+					[attr.id]="id"
+					[disabled]="disabled"
+					maxlength="5"
+					(change)="onChange($event)"
+					type="string"
+					class="bx--time-picker__input-field">
 			</div>
-		</div>
+			<ng-content></ng-content>
 	`
 })
 export class TimePicker {
@@ -39,6 +36,8 @@ export class TimePicker {
 	 * Tracks the total number of selects instantiated. Used to generate unique IDs
 	 */
 	static timePickerCount = 0;
+
+	@HostBinding("class.bx--time-picker") timePicker = true;
 
 	@Input() label;
 	@Input() placeholder = "hh:mm";
