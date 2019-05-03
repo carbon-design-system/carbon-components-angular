dd6a9bf06441075e8dfff0d552d46c5a410dac07
Yousef Ouda <ouda.yousef@gmail.com>
2019-05-03 12:49:46 -0400

    Add label to radio story

--- src/radio/radio.stories.ts
+++ src/radio/radio.stories.ts
@@ -1,6 +1,6 @@
 import { storiesOf, moduleMetadata } from "@storybook/angular";
 import { action } from "@storybook/addon-actions";
-import { withKnobs, boolean } from "@storybook/addon-knobs/angular";
+import { withKnobs, text } from "@storybook/addon-knobs/angular";
 
 import { RadioModule } from "../";
 
@@ -12,21 +12,25 @@ storiesOf("Radio", module).addDecorator(
 	.addDecorator(withKnobs)
 	.add("Basic", () => ({
 		template: `
-		<ibm-radio-group aria-label="radiogroup" [(ngModel)]="radio" (change)="onChange($event)">
-			<ibm-radio
-				value="radio"
-				[checked]="true">
-				zero
-			</ibm-radio>
-			<ibm-radio *ngFor="let radio of manyRadios"
-				[value]="radio.num"
-				[disabled]="radio.disabled">
-				{{radio.num}}
-			</ibm-radio>
-		</ibm-radio-group>
+		<fieldset class="bx--fieldset">
+			<legend class="bx--label">{{label}}</legend>
+			<ibm-radio-group aria-label="radiogroup" [(ngModel)]="radio" (change)="onChange($event)">
+				<ibm-radio
+					value="radio"
+					[checked]="true">
+					zero
+				</ibm-radio>
+				<ibm-radio *ngFor="let radio of manyRadios"
+					[value]="radio.num"
+					[disabled]="radio.disabled">
+					{{radio.num}}
+				</ibm-radio>
+			</ibm-radio-group>
+		</fieldset>
 		`,
 		props: {
 			onChange: action("Radio change"),
+			label: text("Label text", "Radio Button heading"),
 			manyRadios: [
 				{ num: "one" },
 				{ num: "two" },
