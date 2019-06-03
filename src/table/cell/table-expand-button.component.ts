cbdb7ce0490b5dae4330e9a811f7ed7051bea207
Cal Smith <calsmith@protonmail.com>
2019-06-03 14:09:15 -0400

    add expand button column component

---
+++ src/table/cell/table-expand-button.component.ts
@@ -0,0 +1,50 @@
+import {
+	Component,
+	Input,
+	Output,
+	EventEmitter,
+	HostBinding
+} from "@angular/core";
+import { I18n } from "./../../i18n/i18n.module";
+
+@Component({
+	// tslint:disable-next-line: component-selector
+	selector: "[ibmTableExpandButton]",
+	template: `
+		<button
+			*ngIf="expandable"
+			class="bx--table-expand__button"
+			[attr.aria-label]="ariaLabel | async"
+			(click)="expandRow.emit()">
+			<ibm-icon-chevron-right16 innerClass="bx--table-expand__svg"></ibm-icon-chevron-right16>
+		</button>
+	`
+})
+export class TableExpandButton {
+	@Input() expanded = false;
+	@Input() expandable = false;
+
+	@Input()
+	set ariaLabel(value) {
+		if (value) {
+			this._ariaLabel.next(value);
+		}
+	}
+	get ariaLabel() {
+		return this._ariaLabel;
+	}
+
+	@Input() skeleton = false;
+
+	@HostBinding("class.bx--table-expand") expandClass = true;
+
+	@HostBinding("attr.data-previous-value") get previousValue() {
+		return this.expanded ? "collapsed" : null;
+	}
+
+	@Output() expandRow = new EventEmitter<void>();
+
+	protected _ariaLabel = this.i18n.get("TABLE.EXPAND_BUTTON");
+
+	constructor(protected i18n: I18n) { }
+}
