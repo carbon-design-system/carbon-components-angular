8821d392bcbbf5e69eefb2a7972e6ed6f49ba89d
Maged Hennawy <maged.hennawy@ibm.com>
2019-06-24 09:01:48 -0400

    Fix review comments

--- src/ui-shell/header/hamburger.component.ts
+++ src/ui-shell/header/hamburger.component.ts
@@ -1,4 +1,8 @@
-import { Component, Output, EventEmitter, Input } from "@angular/core";
+import { Component,
+	Output,
+	EventEmitter,
+	Input
+} from "@angular/core";
 import { I18n } from "../../i18n/i18n.module";
 
 /**
@@ -11,16 +15,16 @@ import { I18n } from "../../i18n/i18n.module";
 	template: `
 		<button
 			(click)="doClick($event)"
-			[ngClass]="{'bx--header__action--active': selected}"
+			[ngClass]="{'bx--header__action--active': active}"
 			class="bx--header__menu-trigger bx--header__action"
 			[attr.aria-label]="i18n.get('UI_SHELL.HEADER.MENU') | async"
 			[attr.title]="i18n.get('UI_SHELL.HEADER.MENU') | async">
 
-			<svg *ngIf="!selected" aria-hidden="true" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+			<svg *ngIf="!active" aria-hidden="true" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
 				<path d="M4 6h24v2H4zm0 18h24v2H4zm0-9h24v2H4z" />
 			</svg>
 
-			<svg *ngIf="selected" aria-hidden="true" height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+			<svg *ngIf="active" aria-hidden="true" height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
 				<path d="M24 9.4L22.6 8 16 14.6 9.4 8 8 9.4l6.6 6.6L8 22.6 9.4 24l6.6-6.6 6.6 6.6 1.4-1.4-6.6-6.6L24 9.4z" />
 			</svg>
 		</button>
@@ -32,14 +36,14 @@ export class Hamburger {
 	 * @type {boolean}
 	 * @memberof Hamburger
 	 */
-	@Input() selected = false;
+	@Input() active = false;
 
 	/**
 	 * `EventEmitter` to notify parent components of menu click events.
 	 * @type {EventEmitter<Object>}
 	 * @memberof Hamburger
 	 */
-	@Output() onClick: EventEmitter<Object> = new EventEmitter<Object>();
+	@Output() selected: EventEmitter<Object> = new EventEmitter<Object>();
 
 	constructor(public i18n: I18n) { }
 
@@ -48,7 +52,7 @@ export class Hamburger {
 	 * @memberof Hamburger
 	 */
 	public doClick() {
-		this.onClick.emit(this.selected);
+		this.selected.emit(this.active);
 	}
 
 }
