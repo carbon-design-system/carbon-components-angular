8af151946dfa0ba2a3587b45089b1b0652f593bc
Yousef Ouda <ouda.yousef@gmail.com>
2019-06-24 16:06:11 -0400

    chore(storybook): starter app button

--- src/index.stories.ts
+++ src/index.stories.ts
@@ -93,14 +93,23 @@ import { ButtonModule } from "./";
 
 			<h1 class="banner__title"><span class="banner__logo--bold">Carbon</span> Components Angular</h1>
 			<h2 class="banner__subtitle">An Angular implementation of the Carbon Design System</h2>
-			<a
-				ibmButton="secondary"
-				class="banner__btn"
-				style="z-index: 2"
-				href="documentation/index.html"
-				target="_blank">
-				Documentation
-			</a>
+
+			<div style="display:inline">
+				<a ibmButton="secondary"class="banner__btn" href="documentation/index.html" target="_blank">
+					Documentation
+					<svg class="bx--btn__icon" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+						<path d="M12.9 4.7L9.4 1.2C9.3 1.1 9.1 1 9 1H4c-.6 0-1 .4-1 1v12c0 .6.4 1 1 1h8c.6 0 1-.4 1-1V5c0-.1-.1-.3-.1-.3zM9 2.2L11.8 5H9V2.2zM12 14H4V2h4v3c0 .6.4 1 1 1h3v8z"></path>
+						<path d="M5 11h6v1H5zm0-3h6v1H5z"></path>
+					</svg>
+				</a>
+				&nbsp;
+				<a ibmButton="primary" class="banner__btn" href="documentation/index.html" target="_blank">
+					Starter App
+					<svg class="bx--btn__icon" width="20" height="20" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+						<path d="M16 10a6 6 0 0 0-6 6v8a6 6 0 0 0 12 0v-8a6 6 0 0 0-6-6zm-4.25 7.87h8.5v4.25h-8.5zM16 28.25A4.27 4.27 0 0 1 11.75 24v-.13h8.5V24A4.27 4.27 0 0 1 16 28.25zm4.25-12.13h-8.5V16a4.25 4.25 0 0 1 8.5 0zm10.41 3.09L24 13v9.1a4 4 0 0 0 8 0 3.83 3.83 0 0 0-1.34-2.89zM28 24.35a2.25 2.25 0 0 1-2.25-2.25V17l3.72 3.47A2.05 2.05 0 0 1 30.2 22a2.25 2.25 0 0 1-2.2 2.35zM0 22.1a4 4 0 0 0 8 0V13l-6.66 6.21A3.88 3.88 0 0 0 0 22.1zm2.48-1.56L6.25 17v5.1a2.25 2.25 0 0 1-4.5 0 2.05 2.05 0 0 1 .73-1.56zM15 5.5A3.5 3.5 0 1 0 11.5 9 3.5 3.5 0 0 0 15 5.5zm-5.25 0a1.75 1.75 0 1 1 1.75 1.75A1.77 1.77 0 0 1 9.75 5.5zM20.5 2A3.5 3.5 0 1 0 24 5.5 3.5 3.5 0 0 0 20.5 2zm0 5.25a1.75 1.75 0 1 1 1.75-1.75 1.77 1.77 0 0 1-1.75 1.75z"></path>
+					</svg>
+				</a>
+			</div>
 		</section>
 	`,
 	// tslint:enable:max-line-length
@@ -132,7 +141,6 @@ import { ButtonModule } from "./";
 		.banner__btn {
 			margin-top: 20px;
 			border-color: #fff;
-			color: #fff;
 		}
 		.banner__logo--bold, .banner__logo span {
 			font-weight: 600;
