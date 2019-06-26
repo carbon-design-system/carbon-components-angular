8d84e2c9d7e86bcb5aab11877c3c82bfbf88d79b
Cal Smith <calsmith@protonmail.com>
2019-06-26 16:01:10 -0400

    add comment

--- src/datepicker/carbon-flatpickr-month-select.ts
+++ src/datepicker/carbon-flatpickr-month-select.ts
@@ -1,3 +1,14 @@
+/**
+ * This is from carbon-components.
+ * We need it to format the month select according to specs.
+ * Carbon currently doesn't expose this as a seperate package,
+ * and we don't import the carbon-components js (on purpose)
+ * so some copy pasta is required
+ *
+ * ref: https://github.com/carbon-design-system/carbon/blob/
+ * 	f06f38f0c2ef624e409a3d5711e897a79f4c88fc/packages/components/src/components/date-picker/date-picker.js#L52-L123
+ */
+
 const monthToStr = (monthNumber, shorthand, locale) =>
 	locale.months[shorthand ? "shorthand" : "longhand"][monthNumber];
 
