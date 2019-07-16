4a2d0779b2e15c38a76b600e644c46f4c73fc516
Cal Smith <calsmith@protonmail.com>
2019-07-16 13:24:01 -0400

    fix review comments

--- src/breadcrumb/breadcrumb.stories.ts
+++ src/breadcrumb/breadcrumb.stories.ts
@@ -1,16 +1,19 @@
 import { storiesOf, moduleMetadata } from "@storybook/angular";
-import { withKnobs, boolean, number } from "@storybook/addon-knobs/angular";
+import { withKnobs, boolean, number, text } from "@storybook/addon-knobs/angular";
 
 import { BreadcrumbModule, DialogModule, DocumentationModule } from "../";
 import { BreadcrumbItem } from "../breadcrumb/breadcrumb-item.interface";
 
 let breadcrumbItems;
 
-const createBreadcrumbItems = (count: number): Array<BreadcrumbItem> => {
+const createBreadcrumbItems = (count: number, content = "Breadcrumb"): Array<BreadcrumbItem> => {
 	if (breadcrumbItems && count === breadcrumbItems.length) {
 		return breadcrumbItems;
 	}
-	breadcrumbItems = Array(count).fill(0).map((x, i) => ({ content: " Breadcrumb " + (i + 1), href: "#" + (i + 1) }));
+	breadcrumbItems = Array(count).fill(0).map((x, i) => ({
+		content: `${content} ${i + 1}`,
+		href: "#" + (i + 1)
+	}));
 	return breadcrumbItems;
 };
 
@@ -52,30 +55,33 @@ storiesOf("Breadcrumb", module)
 	<ibm-breadcrumb
 		[noTrailingSlash]="noTrailingSlash"
 		[threshold]="threshold"
-		[items]="items(itemCount)">
+		[items]="createBreadcrumbItems(itemCount, content)">
 	</ibm-breadcrumb>`,
 	props: {
 		noTrailingSlash: boolean("noTrailingSlash", true),
 		itemCount: number("itemCount", 10),
 		threshold: number("threshold", 4),
-		items: createBreadcrumbItems
+		content: text("Content for the items", "Breadcrumb"),
+		createBreadcrumbItems
 	}
 }))
 .add("Model with templates", () => ({
 	template: `
 	<ng-template #breadcrumbTemplate let-item>
-		Hello {{ item.content }}
+		{{ templateContent }}{{ item.content }}
 	</ng-template>
 	<ibm-breadcrumb
 		[noTrailingSlash]="noTrailingSlash"
 		[threshold]="threshold"
-		[items]="withTemplate(breadcrumbTemplate, createBreadcrumbItems(itemCount))">
+		[items]="withTemplate(breadcrumbTemplate, createBreadcrumbItems(itemCount, content))">
 	</ibm-breadcrumb>
 	`,
 	props: {
 		noTrailingSlash: boolean("noTrailingSlash", true),
 		itemCount: number("itemCount", 10),
 		threshold: number("threshold", 4),
+		templateContent: text("Content for the template", "Hello "),
+		content: text("Content for the items", "breadcrumb"),
 		createBreadcrumbItems,
 		withTemplate
 	}
