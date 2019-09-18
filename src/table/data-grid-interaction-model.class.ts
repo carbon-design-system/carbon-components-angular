00ea934490810edbca5564cfc287100bb71b5a84
Cal Smith <calsmith@protonmail.com>
2019-09-18 18:09:31 -0400

    add documentation

--- src/table/data-grid-interaction-model.class.ts
+++ src/table/data-grid-interaction-model.class.ts
@@ -7,35 +7,121 @@ import { map } from "rxjs/operators";
 import { TableAdapter } from "./table-adapter.class";
 import { tabbableSelectorIgnoreTabIndex, getFocusElementList } from "./../common/tab.service";
 
+/**
+ * The current and previous position in the grid.
+ *
+ * `current` and `previous` are tuples that follow the `[row, column]` convention.
+ */
 export interface DataGridPosition {
 	current: [number, number];
 	previous: [number, number];
 }
 
+/**
+ * `DataGridInteractionModel` provides centralized control over arbitrary 2d grids, following the w3 specs.
+ *
+ * Refs:
+ *  - https://www.w3.org/TR/wai-aria-practices/examples/grid/dataGrids.html
+ *  - https://www.w3.org/TR/wai-aria-practices/#grid
+ *
+ * Example usage (taken from `table.component`):
+ * ```
+// a standard HTML table
+const table = this.elementRef.nativeElement.querySelector("table") as HTMLTableElement;
+
+// `TableDomAdapter` implements `TableAdapter` and provides a consistent interface to query rows and columns in a table
+const tableAdapter = new TableDomAdapter(table);
+
+// the keydown events that we'll use for keyboard navigation of the table
+const keydownEventStream = fromEvent<KeyboardEvent>(table, "keydown");
+
+// the click events we'll use to ensure focus is updated correctly on click
+const clickEventStream = fromEvent<MouseEvent>(table, "click");
+
+// the `DataGridInteractionModel` instance!
+this.interactionModel = new DataGridInteractionModel(keydownEventStream, clickEventStream, tableAdapter);
+
+// subscribe to the combined position updates
+this.interactionModel.position.subscribe(event => {
+	const [currentRow, currentColumn] = event.current;
+	const [previousRow, previousColumn] = event.previous;
+
+	// query the TableAdapter for the cell at the current row and column ...
+	const currentElement = tableAdapter.getCell(currentRow, currentColumn);
+	// ... and make it focusable it
+	Table.setTabIndex(currentElement, 0);
+
+	// if the model has just initialized don't focus or reset anything
+	if (previousRow === -1 || previousColumn === -1) { return; }
+
+	// query the TableAdapter for the cell at the previous row and column ...
+	const previousElement = tableAdapter.getCell(previousRow, previousColumn);
+	// ... and make it unfocusable (now there is only a single focusable cell)
+	Table.setTabIndex(previousElement, -1);
+
+	// finally, focus the current cell (skipped during initilzation)
+	Table.focus(currentElement);
+});
+ * ```
+ */
 export class DataGridInteractionModel {
+	/**
+	 * An Observable that provides an aggregated view of the `rowIndex` and `columnIndex` Observables
+	 */
 	readonly position: Observable<DataGridPosition>;
+	/**
+	 * An Observable that provides the current and previous row indexes.
+	 */
 	readonly rowIndex: Observable<{ current: number, previous: number }>;
+	/**
+	 * An Observable that provides the current and previous column indexes.
+	 */
 	readonly columnIndex: Observable<{ current: number, previous: number }>;
 
+	/**
+	 * Internal subject to handle changes in row
+	 */
 	protected rowSubject = new BehaviorSubject({ current: 0, previous: -1 });
+	/**
+	 * Internal subject to handle changes in column
+	 */
 	protected columnSubject = new BehaviorSubject({ current: 0, previous: -1 });
 
+	/**
+	 * The latest value emitted by the rowSubject
+	 */
 	protected get currentRow() {
 		return this.rowSubject.getValue().current;
 	}
 
+	/**
+	 * The latest value emitted by the columnSubject
+	 */
 	protected get currentColumn() {
 		return this.columnSubject.getValue().current;
 	}
 
+	/**
+	 * The last column as reported by the adapter
+	 */
 	protected get lastColumn() {
 		return this.tableAdapter.lastColumn;
 	}
 
+	/**
+	 * The last row as reported by the adapter
+	 */
 	protected get lastRow() {
 		return this.tableAdapter.lastRow;
 	}
 
+	/**
+	 * `DataGridInteractionModel` requires knowledge of events, and a representation of your table/grid to be useful.
+	 *
+	 * @param keyboardEventStream an Observable of KeyboardEvents. Should be scoped to the table container.
+	 * @param clickEventStream an Observable of ClickEvents. should only include clicks that take action on items known by the TableAdapter
+	 * @param tableAdapter an instance of a concrete class that implements TableAdapter. The standard carbon table uses TableDomAdapter
+	 */
 	constructor(
 		protected keyboardEventStream: Observable<KeyboardEvent>,
 		protected clickEventStream: Observable<MouseEvent>,
@@ -53,6 +139,15 @@ export class DataGridInteractionModel {
 		this.clickEventStream.subscribe(this.handleClickEvent.bind(this));
 	}
 
+	/**
+	 * Handles moving the position according to the w3 datagrid navigation specs
+	 *
+	 * Refs:
+	 *  - https://www.w3.org/TR/wai-aria-practices/examples/grid/dataGrids.html
+	 *  - https://www.w3.org/TR/wai-aria-practices/#grid
+	 *
+	 * @param event the KeyboardEvent to handle
+	 */
 	handleKeyboardEvent(event: KeyboardEvent) {
 		const currentCell = this.tableAdapter.getCell(this.currentRow, this.currentColumn);
 		switch (event.key) {
@@ -62,14 +157,14 @@ export class DataGridInteractionModel {
 				// add the colspan since getColumnFromCell will return the
 				// first column containing the cell (of N columns it may span)
 				// and we want to navigate to the next "real" column
-				this.goToColumn(this.tableAdapter.getColumnFromCell(currentCell) + currentCell.colSpan);
+				this.goToColumn(this.tableAdapter.findColumnIndex(currentCell) + currentCell.colSpan);
 				break;
 			case "Left": // IE specific value
 			case "ArrowLeft":
 				event.preventDefault();
 				// we only ever need to subtract 1 from the column, since getColumnFromCell returns the
 				// first of N columns containing the cell
-				this.goToColumn(this.tableAdapter.getColumnFromCell(currentCell) - 1);
+				this.goToColumn(this.tableAdapter.findColumnIndex(currentCell) - 1);
 				break;
 			case "Down": // IE specific value
 			case "ArrowDown":
@@ -100,27 +195,51 @@ export class DataGridInteractionModel {
 		}
 	}
 
+	/**
+	 * Handles moving the position to the clicked cell
+	 *
+	 * @param event the MouseEvent to handle
+	 */
 	handleClickEvent(event: MouseEvent) {
 		const cell = (event.target as HTMLElement).closest("td, th") as HTMLTableCellElement;
-		const [rowIndex, cellIndex] = this.tableAdapter.getPositionFromCell(cell);
+		const [rowIndex, cellIndex] = this.tableAdapter.findIndex(cell);
 		this.goTo({ row: rowIndex, column: cellIndex });
 	}
 
+	/**
+	 * Jump to a specific column without changing the row
+	 *
+	 * @param index column to jump to
+	 */
 	goToColumn(index: number) {
 		if (index > this.lastColumn || index < 0) { return; }
 		this.goTo({ row: this.currentRow, column: index});
 	}
 
+	/**
+	 * Jump to a specific row without chaning the column
+	 *
+	 * @param index row to jump to
+	 */
 	goToRow(index: number) {
 		if (index > this.lastRow || index < 0) { return; }
 		this.goTo({row: index, column: this.currentColumn});
 	}
 
+	/**
+	 * Jump to the specified row and column
+	 *
+	 * @param param0 an object that contains `row` and `column` properties
+	 */
 	goTo({row, column}) {
 		this.rowSubject.next({ current: row, previous: this.currentRow });
 		this.columnSubject.next({ current: column, previous: this.currentColumn });
 	}
 
+	/**
+	 * Convenience method to reset the tab indexes on a standard carbon table.
+	 * For custom tables you may want to reset the indexes manually and simply call `.reset()`
+	 */
 	resetTabIndexes() {
 		for (let i = 0; i < this.tableAdapter.lastRow; i++) {
 			const row = this.tableAdapter.getRow(i) as HTMLTableRowElement;
@@ -131,6 +250,13 @@ export class DataGridInteractionModel {
 			}
 		}
 
+		this.reset();
+	}
+
+	/**
+	 * Resets the models focus position
+	 */
+	reset() {
 		this.rowSubject.next({ current: 0, previous: -1 });
 		this.columnSubject.next({ current: 0, previous: -1 });
 	}
--- src/table/table-adapter.class.ts
+++ src/table/table-adapter.class.ts
@@ -1,40 +1,108 @@
-export interface TableCellAdapter {
+/**
+ * An abstract class that represents a cell in a table
+ */
+export abstract class TableCellAdapter {
 	cellIndex: number;
 	colSpan: number;
 }
 
-export interface TableRowAdapter {
+/**
+ * An abstract class that represents a row in a table
+ */
+export abstract class TableRowAdapter {
 	rowIndex: number;
 	cells: HTMLCollection | TableCellAdapter[];
 }
 
+/**
+ * An abstract representation of a table that provides
+ * a standard interface to query 2d tables for cell and row information.
+ */
 export abstract class TableAdapter {
+	/**
+	 * The last acccessible column in the table
+	 */
 	public get lastColumn(): number { return; }
 
+	/**
+	 * The last accessible row in the table
+	 */
 	public get lastRow(): number { return; }
 
+	/**
+	 * Returns a cell from the table
+	 *
+	 * @param row index of the row
+	 * @param column index of the column
+	 */
 	getCell(row: number, column: number): TableCellAdapter { return; }
 
+	/**
+	 * Returns a row from the table
+	 *
+	 * @param row index of the row
+	 */
 	getRow(row: number): TableRowAdapter { return; }
 
-	getColumnFromCell(cell: TableCellAdapter): number { return; }
+	/**
+	 * Finds the column index of a given cell
+	 *
+	 * @param cell the cell to search for
+	 */
+	findColumnIndex(cell: TableCellAdapter): number { return; }
 
-	getRowFromCell(cell: TableCellAdapter): number { return; }
+	/**
+	 * Finds the row index of a given cell
+	 *
+	 * @param cell the cell to search for
+	 */
+	findRowIndex(cell: TableCellAdapter): number { return; }
 
-	getPositionFromCell(cell: TableCellAdapter): [number, number] { return; }
+	/**
+	 * Finds the row and column index of a given cell
+	 *
+	 * @param cell the cell to search for
+	 * @returns a tuple that follows the `[row, column]` convention
+	 */
+	findIndex(cell: TableCellAdapter): [number, number] { return; }
 }
 
+/**
+ * A concrete implementation of `TableAdapter`
+ *
+ * Provides standard and consistent access to table cells and rows
+ */
 export class TableDomAdapter implements TableAdapter {
+	/**
+	 * The last acccessible column in the table
+	 */
 	public get lastColumn() {
 		return this.tableElement.rows[0].cells.length - 1;
 	}
 
+	/**
+	 * The last accessible row in the table
+	 */
 	public get lastRow() {
 		return this.tableElement.rows.length - 1;
 	}
 
+	/**
+	 * `TableDomAdapter` works on a normal HTML table structure.
+	 * Custom tables that don't follow the standard structure should use a custom implementation of `TableAdapter`.
+	 *
+	 * The standard structure allows us to directly query rows for cells and indexes - though we do have to handle colspans specially.
+	 *
+	 * @param tableElement the root HTML table element.
+	 */
 	constructor(public tableElement: HTMLTableElement) { }
 
+	/**
+	 * Returns a cell from the table taking colspans in to account.
+	 *
+	 * @param row index of the row
+	 * @param column index of the column
+	 */
 	getCell(row: number, column: number): HTMLTableCellElement {
 		const baseRow = this.getRow(row);
 		let baseCells = Array.from(baseRow.cells);
@@ -55,12 +123,22 @@ export class TableDomAdapter implements TableAdapter {
 		return baseCells[realIndex];
 	}
 
+	/**
+	 * Returns a row from the table
+	 *
+	 * @param row index of the row
+	 */
 	getRow(row: number): HTMLTableRowElement {
 		return this.tableElement.rows[row];
 	}
 
-	getColumnFromCell(cell: HTMLTableCellElement): number {
-		const row = this.getRow(this.getRowFromCell(cell));
+	/**
+	 * Finds the column index of a given cell
+	 *
+	 * @param cell the cell to search for
+	 */
+	findColumnIndex(cell: HTMLTableCellElement): number {
+		const row = this.getRow(this.findRowIndex(cell));
 		let cellIndex = 0;
 		for (const c of Array.from(row.cells)) {
 			if (c === cell) { break; }
@@ -69,7 +147,12 @@ export class TableDomAdapter implements TableAdapter {
 		return cellIndex;
 	}
 
-	getRowFromCell(cell: HTMLTableCellElement): number {
+	/**
+	 * Finds the row index of a given cell
+	 *
+	 * @param cell the cell to search for
+	 */
+	findRowIndex(cell: HTMLTableCellElement): number {
 		for (const row of Array.from(this.tableElement.rows)) {
 			if (row.contains(cell)) {
 				return row.rowIndex;
@@ -77,7 +160,13 @@ export class TableDomAdapter implements TableAdapter {
 		}
 	}
 
-	getPositionFromCell(cell: HTMLTableCellElement): [number, number] {
-		return [this.getRowFromCell(cell), this.getColumnFromCell(cell)];
+	/**
+	 * Finds the row and column index of a given cell
+	 *
+	 * @param cell the cell to search for
+	 * @returns a tuple that follows the `[row, column]` convention
+	 */
+	findIndex(cell: HTMLTableCellElement): [number, number] {
+		return [this.findRowIndex(cell), this.findColumnIndex(cell)];
 	}
 }
