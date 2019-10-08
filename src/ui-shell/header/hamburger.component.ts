f0f893ea0649e1d71cd66fafdadb8934eb32276b
Donisius Wigie <donisius.wigie@gmail.com>
2019-10-08 13:55:44 -0400

    Change svg path to directive

--- src/ui-shell/header/hamburger.component.ts
+++ src/ui-shell/header/hamburger.component.ts
@@ -19,16 +19,7 @@ import { I18n } from "../../i18n/i18n.module";
 			class="bx--header__menu-trigger bx--header__action bx--header__menu-toggle"
 			[attr.aria-label]="i18n.get('UI_SHELL.HEADER.MENU') | async"
 			[attr.title]="i18n.get('UI_SHELL.HEADER.MENU') | async">
-			<svg focusable="false"
-				preserveAspectRatio="xMidYMid meet"
-				xmlns="http://www.w3.org/2000/svg"
-				width="20"
-				height="20"
-				viewBox="0 0 20 20"
-				aria-hidden="true"
-				style="will-change: transform;">
-				<path d="M2 14.8h16V16H2zm0-3.6h16v1.2H2zm0-3.6h16v1.2H2zM2 4h16v1.2H2z"></path>
-			</svg>
+			<svg ibmIconMenu20></svg>
 		</button>
 	`
 })
