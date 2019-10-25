85150eeea4bde3cc4ba1a5a33343c58121abf4df
Donisius Wigie <donisius.wigie@gmail.com>
2019-10-25 12:11:04 -0400

    Update tests for table

--- src/table/table.component.spec.ts
+++ src/table/table.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, SimpleChanges } from "@angular/core";
 import { TestBed } from "@angular/core/testing";
 
 import { By } from "@angular/platform-browser";
@@ -9,20 +9,33 @@ import { TableHeaderItem } from "./table-header-item.class";
 import { TableItem } from "./table-item.class";
 
 @Component({
-	template: `<ibm-table [model]="tableModel"></ibm-table>`
+	template: `
+		<ibm-table
+			[model]="tableModel"
+			(sort)="simpleSort()"
+			(selectRow)="onChange()"
+			(deselectRow)="onChange()"
+			size="sh"
+			title="title"
+			[showSelectionColumn]="showSelectionColumn">
+		</ibm-table>`
 })
 class TableTest implements OnInit {
 	tableModel = new TableModel();
+	showSelectionColumn = true;
 
 	ngOnInit() {
 		this.tableModel.header = [new TableHeaderItem({data: "Column"})];
 		this.tableModel.data = [
-			[new TableItem({data: "Lorem"})],
-			[new TableItem({data: "ipsum"})],
-			[new TableItem({data: "dolor"})],
-			[new TableItem({data: "sit"})]
+			[new TableItem({data: "0"})],
+			[new TableItem({data: "3"})],
+			[new TableItem({data: "1"})],
+			[new TableItem({data: "2"})]
 		];
 	}
+
+	simpleSort() {}
+	onChange() {}
 }
 
 describe("Table", () => {
@@ -44,7 +57,12 @@ describe("Table", () => {
 		expect(fixture.componentInstance instanceof Table).toBe(true);
 	});
 
-	it("should call the row sort function", () => {});
+	it("should call the row sort function", () => {
+		spyOn(fixture.componentInstance, "simpleSort");
+		tableInstance.nativeElement.querySelector("thead .bx--table-sort").click();
+		fixture.detectChanges();
+		expect(fixture.componentInstance.simpleSort).toHaveBeenCalled();
+	});
 
 	it("should call the row filter function", () => {});
 
@@ -90,5 +108,22 @@ describe("Table", () => {
 		fixture.detectChanges();
 
 		expect(tableInstance.componentInstance.deselectRow.emit).toHaveBeenCalled();
+	});
+
+	it("should set the .bx--data-table--short class", () => {
+		expect(tableInstance.nativeElement.querySelector(".bx--data-table--short")).toBeTruthy();
+	});
+
+	it("should not show checkboxes when showSelectionColumn is false", () => {
+		expect(tableInstance.nativeElement.querySelector("ibm-checkbox")).toBeTruthy();
+
+		fixture.componentInstance.showSelectionColumn = false;
+		fixture.detectChanges();
+
+		expect(tableInstance.nativeElement.querySelector("ibm-checkbox")).not.toBeTruthy();
+	});
+
+	it("should set title to 'title", () => {
+		expect(tableInstance.nativeElement.getAttribute("title")).toBe("title");
 	});
 });
