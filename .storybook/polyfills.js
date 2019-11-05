407daaa2f456015e8e87a570f2b43cefd3ebe3f3
Donisius Wigie <donisius.wigie@gmail.com>
2019-11-05 15:36:26 -0500

    fix(date picker): fix infinite loop occuring in IE11

--- .storybook/polyfills.js
+++ .storybook/polyfills.js
@@ -1,3 +1,19 @@
 import "core-js/es";
 
 import "zone.js/dist/zone";
+
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+  
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+  
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+            return null;
+    };
+}
