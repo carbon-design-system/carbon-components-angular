85a8c1cc3b9dab8e798b16322063ed7211608c76
Luis Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2019-11-07 15:30:46 -0600

    feature(dropdown-list): add ability to set output to emit scroll event

---
+++ src/dropdown/list/scroll-custom-event.interface.ts
@@ -0,0 +1,27 @@
+import { Scroll } from "@angular/router";
+
+/**
+ * A custom structure that represents a custom event to emit when scroll.
+ *
+ * ```typescript
+ * export interface ScrollCustomEvent {
+ * 	atTop: boolean;
+ * 	atBottom: boolean;
+ * 	event: Scroll;
+ * }
+ * ```
+ */
+export interface ScrollCustomEvent {
+	/**
+	 * Flag to communicate if scroll is at the top of container
+	 */
+	atTop: boolean;
+	/**
+	 * Flag to communicate if scroll is at the bottom of container
+	 */
+	atBottom: boolean;
+	/**
+	 * Complete Scroll event available to get any more data required
+	 */
+	event: Scroll;
+}
