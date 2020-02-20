b5863b33105d3d4b3914d8afd45146887354520c
Donisius Wigie <donisius.wigie@gmail.com>
2020-02-20 15:56:34 -0500

    Add back host binding of container class

--- src/table/table-container.component.ts
+++ src/table/table-container.component.ts
@@ -2,10 +2,8 @@ import { Component, HostBinding } from "@angular/core";
 
 @Component({
 	selector: "ibm-table-container",
-	template: `
-		<div [ngClass]="{'bx--data-table-container': containerClass}">
-			<ng-content></ng-content>
-		</div>
-	`
+	template: `<div><ng-content></ng-content></div>`
 })
-export class TableContainer { }
+export class TableContainer {
+	@HostBinding("class.bx--data-table-container") containerClass = true;
+}
