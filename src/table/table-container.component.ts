fd500c0111501447995574838965704e9cf481e4
Donisius Wigie <donisius.wigie@gmail.com>
2020-02-20 16:34:50 -0500

    Add styles to host and remove wrapping div
    Co-authored-by: Cal Smith <callums@ca.ibm.com>
    Co-authored-by: Zvonimir Fras <zvonimir@zvonimirfras.com>

--- src/table/table-container.component.ts
+++ src/table/table-container.component.ts
@@ -2,7 +2,13 @@ import { Component, HostBinding } from "@angular/core";
 
 @Component({
 	selector: "ibm-table-container",
-	template: `<div><ng-content></ng-content></div>`
+	template: `<ng-content></ng-content>`,
+	styles: [`
+		:host {
+			display: block;
+			position: relative;
+		}
+	`]
 })
 export class TableContainer {
 	@HostBinding("class.bx--data-table-container") containerClass = true;
