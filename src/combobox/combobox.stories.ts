88df31b5cc704650b4f7f02978c0be086d1df4bb
Donisius Wigie <donisius.wigie@gmail.com>
2020-03-31 12:25:45 -0400

    fix(combobox): allow selected values to be updated through reactive forms

--- src/combobox/combobox.stories.ts
+++ src/combobox/combobox.stories.ts
@@ -4,6 +4,13 @@ import { withKnobs, text, boolean } from "@storybook/addon-knobs/angular";
 
 import { ComboBoxModule } from "./combobox.module";
 import { DocumentationModule } from "./../documentation-component/documentation.module";
+import {
+	ReactiveFormsModule,
+	FormGroup,
+	FormBuilder,
+	FormControl
+} from '@angular/forms';
+import { Component, OnInit, Input } from "@angular/core";
 
 const getOptions = (override = {}) => {
 	const options = {
@@ -33,11 +40,62 @@ const getOptions = (override = {}) => {
 	return Object.assign({}, options, override);
 };
 
+@Component({
+	selector: "app-reactive-combobox",
+	template: `
+		<form [formGroup]="sampleForm" (ngSubmit)="onSubmit(sampleForm)">
+			<ibm-combo-box
+				formControlName="combobox"
+				[label]="label"
+				[helperText]="helperText"
+				[items]="items">
+				<ibm-dropdown-list></ibm-dropdown-list>
+			</ibm-combo-box>
+			selected: {{ sampleForm.get("combobox").value | json }}
+			<ibm-combo-box
+				style="margin-top: 40px"
+				formControlName="multibox"
+				[label]="label"
+				[helperText]="helperText"
+				type="multi"
+				[items]="items">
+				<ibm-dropdown-list></ibm-dropdown-list>
+			</ibm-combo-box>
+			selected: {{ sampleForm.get("multibox").value | json }}
+		</form>
+	`
+})
+class ReactiveFormsCombobox implements OnInit {
+	public sampleForm: FormGroup;
+	@Input() items = [];
+	@Input() label = "";
+	@Input() helperText = "";
+
+	constructor(private fb: FormBuilder) {}
+
+	ngOnInit() {
+		this.sampleForm = this.fb.group({
+			combobox: new FormControl,
+			multibox: new FormControl
+		});
+
+		this.sampleForm.get("combobox").setValue({ content: "four", selected: true });
+		this.sampleForm.get("multibox").setValue(
+			[
+				{ content: "four", selected: true },
+				{ content: "two", selected: true }
+			]
+		);
+	}
+}
+
 storiesOf("Components|Combobox", module)
 	.addDecorator(
 		moduleMetadata({
+			declarations: [ReactiveFormsCombobox],
 			imports: [
 				ComboBoxModule,
+				ReactiveFormsModule,
 				DocumentationModule
 			]
 		})
@@ -157,6 +215,16 @@ storiesOf("Components|Combobox", module)
 				(submit)="submit($event)">
 				<ibm-dropdown-list></ibm-dropdown-list>
 			</ibm-combo-box>
+		`,
+		props: getOptions()
+	}))
+	.add("With reactive forms", () => ({
+		template: `
+			<app-reactive-combobox
+				[items]="items"
+				[label]="label"
+				[helperText]="helperText">
+			</app-reactive-combobox>
 		`,
 		props: getOptions()
 	}))
