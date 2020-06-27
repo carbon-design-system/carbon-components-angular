a309047bbc9160c35a92fe4dc61e8a182309485e
Cal Smith <calsmith@protonmail.com>
2020-06-27 10:07:30 -0400

    fix storybook

--- src/table/stories/app-custom-table.component.ts
+++ src/table/stories/app-custom-table.component.ts
@@ -8,7 +8,7 @@ import {
 import { TableModel } from "../table-model.class";
 import { TableItem } from "../table-item.class";
 import { TableHeaderItem } from "../table-header-item.class";
-import { clone } from "carbon-components-angular/utils";
+import { clone } from "../../utils/index";
 
 export class CustomHeaderItem extends TableHeaderItem {
 	// used for custom sorting
