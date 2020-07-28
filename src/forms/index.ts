831a58f375bef96b711eed2b03a290dac28321ac
Cal Smith <calsmith@protonmail.com>
2020-07-28 16:38:37 -0400

    fix root and forms exports

--- src/forms/index.ts
+++ src/forms/index.ts
@@ -1,6 +1,35 @@
-export { CheckboxModule } from "carbon-components-angular/checkbox";
-export { ToggleModule } from "carbon-components-angular/toggle";
-export { RadioModule } from "carbon-components-angular/radio";
-export { InputModule } from "carbon-components-angular/input";
-export { ButtonModule } from "carbon-components-angular/button";
-export * from "./forms.module";
+// export the forms module
+export { NFormsModule } from "./forms.module";
+
+// re-export from form related modules and components
+export {
+	CheckboxModule,
+	Checkbox,
+	CheckboxChange,
+	CheckboxState,
+	CheckboxValue
+} from "carbon-components-angular/checkbox";
+export {
+	ToggleModule,
+	Toggle,
+	ToggleChange,
+	ToggleState
+} from "carbon-components-angular/toggle";
+export {
+	RadioModule,
+	Radio,
+	RadioChange,
+	RadioGroup
+} from "carbon-components-angular/radio";
+export {
+	InputModule,
+	Label,
+	TextArea,
+	TextInput
+} from "carbon-components-angular/input";
+export {
+	ButtonModule,
+	Button,
+	ButtonSize,
+	ButtonType
+} from "carbon-components-angular/button";
