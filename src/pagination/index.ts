b5a6de1fc71e2a86aebfb45400db6ac6944aaae6
Gob <gob@Gobs-MacBook-Pro.local>
2020-11-11 15:48:13 -0500

    fix: change export syntax

--- src/pagination/index.ts
+++ src/pagination/index.ts
@@ -1,6 +1,6 @@
 export * from "./pagination-model.class";
 export * from "./pagination.component";
 export * from "./pagination.module";
-export * from "./pagination-nav/pagination-overflow.component";
-export * from "./pagination-nav/pagination-nav-model.class";
-export * from "./pagination-nav/pagination-nav.component";
+export { PaginationOverflow } from "./pagination-nav/pagination-overflow.component";
+export { PaginationNavModel } from "./pagination-nav/pagination-nav-model.class";
+export { PaginationNavTranslations, PaginationNav} from "./pagination-nav/pagination-nav.component";
