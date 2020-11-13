3e347e0288b71381f75cf3e10dcba242cece1826
Luis Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2020-11-12 19:38:53 -0600

    feat(tablestory): add example of use of cancel output

--- src/table/table.stories.ts
+++ src/table/table.stories.ts
@@ -84,6 +84,10 @@ function getModelWithDisabledRows() {
 	return disabledModel;
 }
 
+function printSomething() {
+	console.log("printing something on cancel");
+}
+
 storiesOf("Components|Table", module).addDecorator(
 		moduleMetadata({
 			imports: [
@@ -176,6 +180,7 @@ storiesOf("Components|Table", module).addDecorator(
 				[model]="model"
 				[batchText]="batchText"
 				[size]="size"
+				(cancel)="cancelMethod()"
 				#toolbar>
 				<ibm-table-toolbar-actions>
 					<button ibmButton="primary" [tabindex]="toolbar.selected ? 0 : -1">
@@ -228,6 +233,9 @@ storiesOf("Components|Table", module).addDecorator(
 		</ibm-table-container>
 	`,
 		props: getProps({
+			cancelMethod: function() {
+				console.log("Custom cancel method");
+			},
 			description: text("Description", "With toolbar"),
 			searchModel: text("Search model", "Initial search value"),
 			enableSingleSelect: boolean("Enable single select", false),
