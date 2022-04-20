be27639f5685c3c18854fffd3fee7447db7223b2
Akshat Patel <akshat@live.ca>
2022-04-19 23:03:20 -0400

    Remove support for deprecated features
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/checkbox/checkbox.component.ts
+++ src/checkbox/checkbox.component.ts
@@ -8,7 +8,6 @@ import {
 	Input,
 	Output,
 	ViewChild,
-	HostBinding,
 	HostListener
 } from "@angular/core";
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from "@angular/forms";
@@ -22,22 +21,6 @@ export enum CheckboxState {
 	Indeterminate,
 	Checked,
 	Unchecked
-}
-
-/**
- * Used to emit changes performed on checkbox components.
- *
- * @deprecated since v4
- */
-export class CheckboxChange {
-	/**
-	 * Contains the `Checkbox` that has been changed.
-	 */
-	source: Checkbox;
-	/**
-	 * The state of the `Checkbox` encompassed in the `CheckboxChange` class.
-	 */
-	checked: boolean;
 }
 
 /**
@@ -92,18 +75,6 @@ export class Checkbox implements ControlValueAccessor, AfterViewInit {
 	static checkboxCount = 0;
 
 	/**
-	 * Size of the checkbox.
-	 *
-	 * @deprecated since v4
-	 */
-	@Input() size: "sm" | "md" = "md";
-	/**
-	 * Set to `true` for checkbox to be rendered with nested styles.
-	 *
-	 * @deprecated since v4
-	 */
-	@Input() nested: boolean;
-	/**
 	 * Set to `true` for checkbox to be rendered without any classes on the host element.
 	 */
 	@Input() inline = false;
@@ -135,41 +106,10 @@ export class Checkbox implements ControlValueAccessor, AfterViewInit {
 	 * Sets the value attribute on the `input` element.
 	 */
 	@Input() value: CheckboxValue;
-	/**
-	 * Used to set the `aria-label` attribute on the input element.
-	 *
-	 * @deprecated since v4 use the `ariaLabel` input instead
-	 */
 	// tslint:disable-next-line:no-input-rename
-	@Input("aria-label") set ariaLabel(value: string) {
-		this._ariaLabel = value;
-	}
-
-	get ariaLabel() {
-		return this._ariaLabel;
-	}
-
-	// TODO: drop the `_`
+	@Input("ariaLabel") ariaLabel = "";
 	// tslint:disable-next-line:no-input-rename
-	@Input("ariaLabel") _ariaLabel = "";
-
-	/**
-	 * Used to set the `aria-labelledby` attribute on the input element.
-	 *
-	 * @deprecated since v4 use the `ariaLabelledby` input instead
-	 */
-	// tslint:disable-next-line:no-input-rename
-	@Input("aria-labelledby") set ariaLabelledby(value: string) {
-		this._ariaLabelledby = value;
-	}
-
-	get ariaLabelledby() {
-		return this._ariaLabelledby;
-	}
-
-	// TODO: drop the `_`
-	// tslint:disable-next-line:no-input-rename
-	@Input("ariaLabelledby") _ariaLabelledby: string;
+	@Input("ariaLabelledby") ariaLabelledby: string;
 
 	/**
 	 * Set the checkbox's indeterminate state to match the parameter and transition the view to reflect the change.
@@ -223,13 +163,6 @@ export class Checkbox implements ControlValueAccessor, AfterViewInit {
 	 * Emits click event.
 	 */
 	@Output() click = new EventEmitter<void>();
-	/**
-	 * Emits event notifying other classes when a change in state occurs on a checkbox after a
-	 * click.
-	 *
-	 * @deprecated since v4 use `checked` and `checkedChange` instead
-	 */
-	@Output() change = new EventEmitter<any>();
 
 	/**
 	 * Emits an event when the value of the checkbox changes.
@@ -365,13 +298,6 @@ export class Checkbox implements ControlValueAccessor, AfterViewInit {
 	 * Creates instance of `CheckboxChange` used to propagate the change event.
 	 */
 	emitChangeEvent() {
-		/* begin deprecation */
-		let event = new CheckboxChange();
-		event.source = this;
-		event.checked = this.checked;
-		this.change.emit(event);
-		/* end deprecation */
-
 		this.checkedChange.emit(this.checked);
 		this.propagateChange(this.checked);
 	}
