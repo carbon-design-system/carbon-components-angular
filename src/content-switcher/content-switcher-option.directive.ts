cba1f8262791640fbc1c47222fb8ee342fd529fe
Licen-it <35496522+Licen-it@users.noreply.github.com>
2022-05-10 16:49:56 +0100

    fix(ContentSwitcherOption): wrapping content of the option in span with bx--content-switcher__label class

--- src/content-switcher/content-switcher-option.directive.ts
+++ src/content-switcher/content-switcher-option.directive.ts
@@ -4,13 +4,15 @@ import {
 	Input,
 	HostListener,
 	Output,
-	EventEmitter
+	EventEmitter,
+	ElementRef,
+	OnInit
 } from "@angular/core";
 
 @Directive({
 	selector: "[ibmContentOption]"
 })
-export class ContentSwitcherOption {
+export class ContentSwitcherOption implements OnInit {
 	/**
 	 * Used to activate the option. Only one option may be `active` at a time
 	 */
@@ -49,6 +51,8 @@ export class ContentSwitcherOption {
 
 	protected _active = false;
 
+	constructor(private hostElement: ElementRef) {}
+
 	@HostListener("click", ["$event"])
 	hostClick(event: MouseEvent) {
 		this.onClick.emit(event);
@@ -65,5 +69,18 @@ export class ContentSwitcherOption {
 		if (this.active) { return; }
 		this.active = true;
 		this.selected.emit(true);
+	}
+
+	/*
+	* encapsulating the content in a span with bx--content-switcher__label class
+	* to mimic what is done in the react version
+	*/
+	ngOnInit(): void {
+		const hostNativeElement = (this.hostElement.nativeElement as HTMLElement);
+		const spanWrapper = document.createElement("span");
+		spanWrapper.className = "bx--content-switcher__label";
+		spanWrapper.innerHTML = hostNativeElement.innerHTML;
+		hostNativeElement.innerHTML = "";
+		hostNativeElement.appendChild(spanWrapper);
 	}
 }
