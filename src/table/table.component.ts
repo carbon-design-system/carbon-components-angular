3ddeb2df3e8dff6c7b677447f7b0962b4489d3f6
Yingle Jia <jiay@us.ibm.com>
2022-06-02 12:58:39 -0400

    fix(table): enable horizontal scroll in table

--- src/table/table.component.ts
+++ src/table/table.component.ts
@@ -173,74 +173,76 @@ import { TableRowSize } from "./table.types";
 @Component({
 	selector: "ibm-table",
 	template: `
-	<table
-		ibmTable
-		[sortable]="sortable"
-		[noBorder]="noBorder"
-		[ngClass]="{'bx--data-table--sticky-header': stickyHeader}"
-		[size]="size"
-		[striped]="striped"
-		[skeleton]="skeleton">
-		<thead
-			ibmTableHead
+	<div class="bx--data-table-content">
+		<table
+			ibmTable
 			[sortable]="sortable"
-			(deselectAll)="onDeselectAll()"
-			(selectAll)="onSelectAll()"
-			(sort)="doSort($event)"
-			[checkboxHeaderLabel]="getCheckboxHeaderLabel()"
-			[filterTitle]="getFilterTitle()"
-			[model]="model"
+			[noBorder]="noBorder"
+			[ngClass]="{'bx--data-table--sticky-header': stickyHeader}"
 			[size]="size"
-			[selectAllCheckbox]="selectAllCheckbox"
-			[selectAllCheckboxSomeSelected]="selectAllCheckboxSomeSelected"
-			[showSelectionColumn]="showSelectionColumn"
-			[enableSingleSelect]="enableSingleSelect"
-			[skeleton]="skeleton"
-			[sortAscendingLabel]="sortAscendingLabel"
-			[sortDescendingLabel]="sortDescendingLabel"
-			[stickyHeader]="stickyHeader">
-		</thead>
-		<tbody
-			ibmTableBody
-			(deselectRow)="onSelectRow($event)"
-			(scroll)="onScroll($event)"
-			(selectRow)="onSelectRow($event)"
-			[checkboxRowLabel]="getCheckboxRowLabel()"
-			[enableSingleSelect]="enableSingleSelect"
-			(rowClick)="onRowClick($event)"
-			[expandButtonAriaLabel]="expandButtonAriaLabel"
-			[model]="model"
-			[size]="size"
-			[ngStyle]="{'overflow-y': 'scroll'}"
-			[selectionLabelColumn]="selectionLabelColumn"
-			[showSelectionColumn]="showSelectionColumn"
-			[skeleton]="skeleton"
-			*ngIf="!noData; else noDataTemplate">
-		</tbody>
-		<ng-template #noDataTemplate><ng-content></ng-content></ng-template>
-		<tfoot>
-			<ng-template
-				[ngTemplateOutlet]="footerTemplate">
-			</ng-template>
-			<tr *ngIf="this.model.isLoading">
-				<td class="table_loading-indicator">
-					<div class="bx--loading bx--loading--small">
-						<svg class="bx--loading__svg" viewBox="-75 -75 150 150">
-							<circle class="bx--loading__stroke" cx="0" cy="0" r="37.5" />
-						</svg>
-					</div>
-				</td>
-			</tr>
-			<tr *ngIf="this.model.isEnd">
-				<td class="table_end-indicator">
-					<h5>{{getEndOfDataText() | async}}</h5>
-					<button (click)="scrollToTop($event)" class="btn--secondary-sm">
-						{{getScrollTopText() | async}}
-					</button>
-				</td>
-			</tr>
-		</tfoot>
-	</table>
+			[striped]="striped"
+			[skeleton]="skeleton">
+			<thead
+				ibmTableHead
+				[sortable]="sortable"
+				(deselectAll)="onDeselectAll()"
+				(selectAll)="onSelectAll()"
+				(sort)="doSort($event)"
+				[checkboxHeaderLabel]="getCheckboxHeaderLabel()"
+				[filterTitle]="getFilterTitle()"
+				[model]="model"
+				[size]="size"
+				[selectAllCheckbox]="selectAllCheckbox"
+				[selectAllCheckboxSomeSelected]="selectAllCheckboxSomeSelected"
+				[showSelectionColumn]="showSelectionColumn"
+				[enableSingleSelect]="enableSingleSelect"
+				[skeleton]="skeleton"
+				[sortAscendingLabel]="sortAscendingLabel"
+				[sortDescendingLabel]="sortDescendingLabel"
+				[stickyHeader]="stickyHeader">
+			</thead>
+			<tbody
+				ibmTableBody
+				(deselectRow)="onSelectRow($event)"
+				(scroll)="onScroll($event)"
+				(selectRow)="onSelectRow($event)"
+				[checkboxRowLabel]="getCheckboxRowLabel()"
+				[enableSingleSelect]="enableSingleSelect"
+				(rowClick)="onRowClick($event)"
+				[expandButtonAriaLabel]="expandButtonAriaLabel"
+				[model]="model"
+				[size]="size"
+				[ngStyle]="{'overflow-y': 'scroll'}"
+				[selectionLabelColumn]="selectionLabelColumn"
+				[showSelectionColumn]="showSelectionColumn"
+				[skeleton]="skeleton"
+				*ngIf="!noData; else noDataTemplate">
+			</tbody>
+			<ng-template #noDataTemplate><ng-content></ng-content></ng-template>
+			<tfoot>
+				<ng-template
+					[ngTemplateOutlet]="footerTemplate">
+				</ng-template>
+				<tr *ngIf="this.model.isLoading">
+					<td class="table_loading-indicator">
+						<div class="bx--loading bx--loading--small">
+							<svg class="bx--loading__svg" viewBox="-75 -75 150 150">
+								<circle class="bx--loading__stroke" cx="0" cy="0" r="37.5" />
+							</svg>
+						</div>
+					</td>
+				</tr>
+				<tr *ngIf="this.model.isEnd">
+					<td class="table_end-indicator">
+						<h5>{{getEndOfDataText() | async}}</h5>
+						<button (click)="scrollToTop($event)" class="btn--secondary-sm">
+							{{getScrollTopText() | async}}
+						</button>
+					</td>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
 	`
 })
 export class Table implements AfterViewInit, OnDestroy {
