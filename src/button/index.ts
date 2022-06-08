ed8917242c07436cb512836c992f67677ef99000
Akshat Patel <akshat@live.ca>
2022-06-07 23:57:52 -0400

    Implement icon button component
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,4 +1,6 @@
 export { Button } from "./button.directive";
 export { ButtonSet } from "./button-set.component";
+export { IconButton } from "./icon-button.component";
+export { BaseIconButton } from "./base-icon-button.component";
 export { ButtonModule } from "./button.module";
 export { ButtonSize, ButtonType, DangerButtonType } from "./button.types";
