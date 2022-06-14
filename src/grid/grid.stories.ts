3d9db267be30df0ee47c0bce012bb1fc5c6319cd
Akshat Patel <akshat@live.ca>
2022-06-14 19:36:48 -0400

    Create basic css grid story
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/grid/grid.stories.ts
+++ src/grid/grid.stories.ts
@@ -1,8 +1,7 @@
 import { storiesOf, moduleMetadata } from "@storybook/angular";
-import { action } from "@storybook/addon-actions";
-import { withKnobs, boolean, object } from "@storybook/addon-knobs/angular";
+import { withKnobs, boolean } from "@storybook/addon-knobs/angular";
 
-import { GridModule } from "../";
+import { GridModule } from "./";
 import { DocumentationModule } from "../documentation-component/documentation.module";
 
 storiesOf("Components|Grid", module)
@@ -12,7 +11,7 @@ storiesOf("Components|Grid", module)
 		})
 	)
 	.addDecorator(withKnobs)
-	.add("Basic", () => ({
+	.add("Basic flex grid", () => ({
 		template: `
         	<div ibmGrid [condensed]="gridCondensed">
 				<div
@@ -29,8 +28,24 @@ storiesOf("Components|Grid", module)
 		`,
 		props: {
 			gutter: boolean("Gutter", true),
-			leftGutter: boolean("Left gutter", true),
-			rightGutter: boolean("Right gutter", true),
+			rowCondensed: boolean("Row condensed", false),
+			gridCondensed: boolean("Grid condensed", false)
+		},
+		styles: [`@use '@carbon/styles/scss/grid/flexbox';`]
+	}))
+	.add("Basic css grid", () => ({
+		template: `
+        	<div ibmGrid [useCssGrid]="true">
+				<div ibmCol class="custom-class-example" [columnNumbers]="{'md': 2, 'sm': 12}">First Column</div>
+				<div ibmCol class="custom-class-example" [columnNumbers]="{'md': 2, 'sm': 12}">Second column</div>
+				<div ibmCol class="custom-class-example" [columnNumbers]="{'md': 2, 'sm': 12}">Third Column</div>
+				<div ibmCol>
+					<div ibmGrid>Should be css subgrid </div>
+				</div>
+         	</div>
+		`,
+		props: {
+			gutter: boolean("Gutter", true),
 			rowCondensed: boolean("Row condensed", false),
 			gridCondensed: boolean("Grid condensed", false)
 		}
