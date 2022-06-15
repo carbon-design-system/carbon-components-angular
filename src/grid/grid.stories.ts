2342c8aca675657e2f0970399bb2b7f270588a00
Akshat Patel <akshat@live.ca>
2022-06-15 19:31:53 -0400

    Use tabs instead of space in templates
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/grid/grid.stories.ts
+++ src/grid/grid.stories.ts
@@ -13,42 +13,36 @@ storiesOf("Components|Grid", module)
 	.addDecorator(withKnobs)
 	.add("Basic flex grid", () => ({
 		template: `
-        	<div ibmGrid [condensed]="gridCondensed">
+			<div ibmGrid [condensed]="gridCondensed">
 				<div
 					ibmRow
 					[gutter]="gutter"
 					[leftGutter]="leftGutter"
 					[rightGutter]="rightGutter"
 					[condensed]="rowCondensed">
-              		<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">First Column</div>
+					<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">First Column</div>
 					<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">Second column</div>
 					<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">Third Column</div>
-            	</div>
-         	</div>
-		`,
-		props: {
-			gutter: boolean("Gutter", true),
-			rowCondensed: boolean("Row condensed", false),
-			gridCondensed: boolean("Grid condensed", false)
-		},
-		styles: [`@use '@carbon/styles/scss/grid/flexbox';`]
-	}))
-	.add("Basic css grid", () => ({
-		template: `
-        	<div ibmGrid [useCssGrid]="true">
-				<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">First Column</div>
-				<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">Second column</div>
-				<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">Third Column</div>
-				<div ibmCol>
-					<div ibmGrid>Should be css subgrid </div>
 				</div>
-         	</div>
+			</div>
 		`,
 		props: {
 			gutter: boolean("Gutter", true),
 			rowCondensed: boolean("Row condensed", false),
 			gridCondensed: boolean("Grid condensed", false)
 		}
+	}))
+	.add("Basic css grid", () => ({
+		template: `
+			<div ibmGrid [useCssGrid]="true">
+				<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">First Column</div>
+				<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">Second column</div>
+				<div ibmCol class="custom-class-example" [columnNumbers]="{md: 2, sm: 12}">Third Column</div>
+				<div ibmCol>
+					<div ibmGrid>Should be css subgrid</div>
+				</div>
+			</div>
+		`
 	}))
 	.add("Documentation", () => ({
 		template: `
