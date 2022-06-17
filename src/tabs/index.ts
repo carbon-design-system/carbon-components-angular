1c5b5a4d0413d1dace1f436fb96d74a2ea411a19
Akshat Patel <akshat@live.ca>
2022-06-17 10:39:29 -0400

    Explicitly export to resolve ambiguity
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/tabs/index.ts
+++ src/tabs/index.ts
@@ -1,7 +1,8 @@
-export * from "./tab-header-group.component";
-export * from "./tab-header.directive";
-export * from "./tab-headers.component";
-export * from "./tab.component";
-export * from "./tabs.component";
-export * from "./tab-skeleton.component";
+export { BaseTabHeader } from "./base-tab-header.component";
+export { TabHeaderGroup } from "./tab-header-group.component";
+export { TabHeader } from "./tab-header.directive";
+export { TabHeaders } from "./tab-headers.component";
+export { Tab } from "./tab.component";
+export { Tabs } from "./tabs.component";
+export { TabSkeleton } from "./tab-skeleton.component";
 export * from "./tabs.module";
