418f8bee383b58cd798df9a6471ce9ebd09b662c
Akshat Patel <akshat@live.ca>
2022-06-27 22:53:09 -0400

    Configure storybook toolbar parameters
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,12 @@ module.exports = {
 	],
 	"addons": [
 		"@storybook/addon-links",
-		"@storybook/addon-essentials"
+		{
+			name: "@storybook/addon-essentials",
+			options: {
+				backgrounds: false
+			}
+		}
 	],
 	"framework": "@storybook/angular",
 	"core": {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,46 @@
 import "./preview.js";
+import { breakpoints } from '@carbon/layout';
+
+// Set carbon viewports options
+export const parameters = {
+	viewport: {
+		viewports: {
+			sm: {
+				name: 'Small',
+				styles: {
+					width: breakpoints.sm.width,
+					height: '100%',
+				},
+			},
+			md: {
+				name: 'Medium',
+				styles: {
+					width: breakpoints.md.width,
+					height: '100%',
+				},
+			},
+			lg: {
+				name: 'Large',
+				styles: {
+					width: breakpoints.lg.width,
+					height: '100%',
+				},
+			},
+			xlg: {
+				name: 'X-Large',
+				styles: {
+					width: breakpoints.xlg.width,
+					height: '100%',
+				},
+			},
+			Max: {
+				name: 'Max',
+				styles: {
+					width: breakpoints.max.width,
+					height: '100%',
+				},
+			},
+		},
+	}
+};
+
