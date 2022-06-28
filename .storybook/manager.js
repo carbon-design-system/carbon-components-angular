044a355f0b901dd710638cbaf4049db31833d162
Akshat Patel <akshat@live.ca>
2022-06-27 22:55:35 -0400

    Set theme configuration
    Signed-off-by: Akshat Patel <akshat@live.ca>

---
+++ .storybook/manager.js
@@ -0,0 +1,14 @@
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming';
+
+addons.setConfig({
+	theme: create({
+		base: 'light',
+		fontBase: "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif",
+		fontCode:
+			"'IBM Plex Mono', Menlo, 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', Courier, monospace",
+		brandTitle: `carbon components angular`,
+		brandUrl:
+			'https://github.com/carbon-design-system/carbon/tree/main/packages/react',
+	})
+});
