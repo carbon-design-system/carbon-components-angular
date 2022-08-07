2fb822130db3443acae3242a3b0a3686fef3ba2b
Kaushal Savani <kaushal.savani@ibm.com>
2022-08-07 14:28:08 +0530

    fix(table): set focus on sort table header button
    Set focus on table header button on sorting
    Fix tab focus on table header with sort button

--- src/table/table.component.ts
+++ src/table/table.component.ts
@@ -274,9 +274,7 @@ export class Table implements AfterViewInit, OnDestroy {
 
 	static setTabIndex(element: HTMLElement, index: -1 | 0) {
 		const focusElementList = getFocusElementList(element, tabbableSelectorIgnoreTabIndex);
-		if (element.firstElementChild && element.firstElementChild.classList.contains("bx--table-sort") && focusElementList.length > 1) {
-			focusElementList[1].tabIndex = index;
-		} else if (focusElementList.length > 0) {
+		if (element.firstElementChild && element.firstElementChild.classList.contains("bx--table-sort") && focusElementList.length > 0) {
 			focusElementList[0].tabIndex = index;
 		} else {
 			element.tabIndex = index;
@@ -285,9 +283,7 @@ export class Table implements AfterViewInit, OnDestroy {
 
 	static focus(element: HTMLElement) {
 		const focusElementList = getFocusElementList(element, tabbableSelectorIgnoreTabIndex);
-		if (element.firstElementChild && element.firstElementChild.classList.contains("bx--table-sort") && focusElementList.length > 1) {
-			focusElementList[1].focus();
-		} else if (focusElementList.length > 0) {
+		if (element.firstElementChild && element.firstElementChild.classList.contains("bx--table-sort") && focusElementList.length > 0) {
 			focusElementList[0].focus();
 		} else {
 			element.focus();
