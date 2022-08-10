4f07dc8f4983fe14cb716ea90646d8d68d7c4bc9
Akshat Patel <akshat@live.ca>
2022-08-10 12:30:11 -0400

    docs: Split into multiple stories
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/accordion/accordion.stories.ts
+++ src/accordion/accordion.stories.ts
@@ -2,23 +2,22 @@
 
 import { moduleMetadata } from "@storybook/angular";
 import { Story, Meta } from "@storybook/angular/types-6-0";
-import { AccordionModule, Accordion, AccordionItem } from "./";
+import { DocumentationModule } from "../documentation-component/documentation.module";
+import { AccordionModule, Accordion } from "./";
 
 export default {
 	title: "Components/Accordion",
 	decorators: [
 		moduleMetadata({
-			imports: [AccordionModule]
+			imports: [AccordionModule, DocumentationModule]
 		})
-	],
-	component: Accordion,
-	subcomponents: { AccordionItem }
+	]
 } as Meta;
 
-const Template: Story = (args) => ({
+const Template: Story<Accordion> = (args) => ({
 	props: args,
 	template: `
-		<ibm-accordion [size]="size" [align]="align" [skeleton]="skeleton">
+		<ibm-accordion [align]="align">
 			<ibm-accordion-item title="Section 1 title" (selected)="selected($event)">
 				Lorem ipsum dolor sit amet, \
 				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
@@ -37,39 +36,68 @@ const Template: Story = (args) => ({
 				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
 				ullamco laboris nisi ut aliquip ex ea commodo consequat.
 			</ibm-accordion-item>
-			<ibm-accordion-item [title]="title" (selected)="selected($event)">
-				Lorem ipsum dolor sit amet, \
-				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
-				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
-				ullamco laboris nisi ut aliquip ex ea commodo consequat.
-			</ibm-accordion-item>
-			<ibm-accordion-item [title]="titleWithContext" [context]="{ index: 5 }" (selected)="selected($event)">
-				Lorem ipsum dolor sit amet, \
-				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
-				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
-				ullamco laboris nisi ut aliquip ex ea commodo consequat.
-			</ibm-accordion-item>
 		</ibm-accordion>
-
-		<ng-template #title>
-			<p class="cds--accordion__title">Template title</p>
-		</ng-template>
-		<ng-template #titleWithContext let-index="index">
-			<p class="cds--accordion__title">Template context title {{ index }}</p>
-		</ng-template>
 	`
 });
 export const Basic = Template.bind({});
 Basic.argTypes = {
 	align: {
 		options: ["start", "end"],
-		control: { type: "radio" },
-		defaultValue: "end"
-	},
-	size: {
-		options: ["sm", "md", "lg"],
-		control: { type: "radio" },
-		defaultValue: "md"
+		control: { type: "radio" }
 	},
 	selected: { action: "Selected" }
 };
+
+const WithTitleTemplate: Story<Accordion> = (args) => ({
+	props: args,
+	template: `
+		<div style="width: 500px">
+			<ibm-accordion>
+				<ibm-accordion-item [title]="title" (selected)="selected($event)">
+					Lorem ipsum dolor sit amet, \
+					consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</ibm-accordion-item>
+				<ibm-accordion-item [title]="titleWithContext" [context]="{ index: 2 }" (selected)="selected($event)">
+					Lorem ipsum dolor sit amet, \
+					consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore \
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</ibm-accordion-item>
+			</ibm-accordion>
+		</div>
+		<ng-template #title>
+			<p class="cds--accordion__title">Section 1 title</p>
+		</ng-template>
+		<ng-template #titleWithContext let-index="index">
+			<p class="cds--accordion__title">Section {{ index }} title</p>
+		</ng-template>
+	`
+});
+export const WithTitle = WithTitleTemplate.bind({});
+WithTitle.parameters = {
+	controls: {
+		disabled: true
+	}
+}
+
+const WithSkeleton: Story<Accordion> = (args) => ({
+	props: args,
+	template: `
+		<div style="width: 500px">
+			<ibm-accordion skeleton="true">
+				<ibm-accordion-item expanded="true"></ibm-accordion-item>
+				<ibm-accordion-item></ibm-accordion-item>
+				<ibm-accordion-item></ibm-accordion-item>
+				<ibm-accordion-item></ibm-accordion-item>
+			</ibm-accordion>
+		</div>
+	`
+});
+export const Skeleton = WithSkeleton.bind({});
+WithSkeleton.parameters = {
+	controls: {
+		disabled: true
+	}
+}
