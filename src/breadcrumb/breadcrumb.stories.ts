0dd4122d207a6d0e0b75d147e30a2aa37d3cfcd8
Akshat Patel <akshat@live.ca>
2022-08-11 22:57:26 -0400

    Add additional stories
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/breadcrumb/breadcrumb.stories.ts
+++ src/breadcrumb/breadcrumb.stories.ts
@@ -5,8 +5,24 @@ import { Story, Meta } from "@storybook/angular/types-6-0";
 import {
 	BreadcrumbModule,
 	Breadcrumb,
-	BreadcrumbItemComponent
+	BreadcrumbItemComponent,
+	BreadcrumbItem
 } from "./";
+
+let breadcrumbItems;
+
+const createBreadcrumbItems = (count: number, content = "Breadcrumb"): Array<BreadcrumbItem> => {
+	if (breadcrumbItems && count === breadcrumbItems.length) {
+		return breadcrumbItems;
+	}
+	breadcrumbItems = Array(count).fill(0).map((x, i) => ({
+		content: `${content} ${i + 1}`,
+		href: "#" + (i + 1)
+	}));
+	return breadcrumbItems;
+};
+
+const withTemplate = (templateRef, items) => items.map(item => Object.assign(item, { template: templateRef }));
 
 export default {
 	title: "Components/Breadcrumb",
@@ -19,6 +35,11 @@ export default {
 		noTrailingSlash: {
 			type: "boolean",
 			defaultValue: true
+		},
+		skeleton: {
+			type: "boolean",
+			defaultValue: false,
+			control: false
 		}
 	},
 	component: Breadcrumb,
@@ -28,7 +49,7 @@ export default {
 const Template: Story<Breadcrumb> = (args) => ({
 	props: args,
 	template: `
-		<ibm-breadcrumb [skeleton]="true" [noTrailingSlash]="noTrailingSlash">
+		<ibm-breadcrumb [noTrailingSlash]="noTrailingSlash">
 			<ibm-breadcrumb-item href="#1">
 				Breadcrumb 1
 			</ibm-breadcrumb-item>
@@ -42,11 +63,77 @@ const Template: Story<Breadcrumb> = (args) => ({
 	`
 });
 export const Basic = Template.bind({});
-Basic.argsTypes = {
-	skeleton: {
-		type: "boolean",
-		defaultValue: false
+
+const CurrentPageTemplate: Story<Breadcrumb> = (args) => ({
+	props: args,
+	template: `
+		<ibm-breadcrumb>
+			<ibm-breadcrumb-item href="#1">
+				Breadcrumb 1
+			</ibm-breadcrumb-item>
+			<ibm-breadcrumb-item href="#2">
+				Breadcrumb 2
+			</ibm-breadcrumb-item>
+			<ibm-breadcrumb-item current="true" href="#3">
+				Breadcrumb 3
+			</ibm-breadcrumb-item>
+		</ibm-breadcrumb>
+	`
+});
+export const CurrentPage = CurrentPageTemplate.bind({});
+CurrentPage.parameters = {
+	controls: {
+		disabled: true
 	}
+};
+
+const ModelTemplate: Story<Breadcrumb> = (args) => {
+	// Load functions into story
+	args.createBreadcrumbItems = createBreadcrumbItems;
+	return {
+		props: args,
+		template: `
+			<ibm-breadcrumb
+				[noTrailingSlash]="noTrailingSlash"
+				[threshold]="threshold"
+				[items]="createBreadcrumbItems(itemCount, content)">
+			</ibm-breadcrumb>
+		`
+	};
+};
+export const Model = ModelTemplate.bind({});
+Model.args = {
+	itemCount: 10,
+	threshold: 4,
+	templateContent: "Hello",
+	content: "breadcrumb"
+};
+
+const ModelWTemplate: Story<Breadcrumb> = (args) => {
+	// Load functions into story
+	args.createBreadcrumbItems = createBreadcrumbItems;
+	args.withTemplate = withTemplate;
+
+	return {
+		props: args,
+		template: `
+			<ng-template #breadcrumbTemplate let-item>
+				{{ templateContent }}{{ item.content }}
+			</ng-template>
+			<ibm-breadcrumb
+				[noTrailingSlash]="noTrailingSlash"
+				[threshold]="threshold"
+				[items]="withTemplate(breadcrumbTemplate, createBreadcrumbItems(itemCount, content))">
+			</ibm-breadcrumb>
+		`
+	};
+};
+export const ModelWithTemplate = ModelWTemplate.bind({});
+ModelWithTemplate.args = {
+	itemCount: 10,
+	threshold: 4,
+	templateContent: "Hello",
+	content: "breadcrumb"
 };
 
 const WithSkeleton: Story<Breadcrumb> = (args) => ({
@@ -61,4 +148,9 @@ const WithSkeleton: Story<Breadcrumb> = (args) => ({
 	`
 });
 export const Skeleton = WithSkeleton.bind({});
+Skeleton.parameters = {
+	controls: {
+		disabled: true
+	}
+};
 
