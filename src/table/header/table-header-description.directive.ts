92c8cdac158855ceb40106d46c44bb292597e4ef
Akshat Patel <akshat@live.ca>
2022-09-22 23:37:04 -0400

    fix: automate setting aria-labelledby & aria-describbedby in table
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/table/header/table-header-description.directive.ts
+++ src/table/header/table-header-description.directive.ts
@@ -1,8 +1,12 @@
-import { Directive, HostBinding } from "@angular/core";
+import { Directive, HostBinding, Input } from "@angular/core";
 
 @Directive({
 	selector: "[ibmTableHeaderDescription]"
 })
 export class TableHeaderDescription {
+	static counter = 0;
+
+	@Input() id = `table-description-${TableHeaderDescription.counter++}`;
 	@HostBinding("class.bx--data-table-header__description") descriptionClass = true;
+	@HostBinding("attr.id") setId = this.id;
 }
--- src/table/header/table-header-title.directive.ts
+++ src/table/header/table-header-title.directive.ts
@@ -1,8 +1,12 @@
-import { Directive, HostBinding } from "@angular/core";
+import { Directive, HostBinding, Input } from "@angular/core";
 
 @Directive({
 	selector: "[ibmTableHeaderTitle]"
 })
 export class TableHeaderTitle {
+	static counter = 0;
+
+	@Input() id = `table-title-${TableHeaderTitle.counter++}`;
 	@HostBinding("class.bx--data-table-header__title") titleClass = true;
+	@HostBinding("attr.id") setId = this.id;
 }
