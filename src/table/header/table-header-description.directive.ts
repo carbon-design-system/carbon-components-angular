9d6d2b374eedf2069c8faabc42847bdb172ebac3
Akshat Patel <akshat@live.ca>
2022-09-23 14:49:38 -0400

    fix: Combine HostBinding & Input var
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/table/header/table-header-description.directive.ts
+++ src/table/header/table-header-description.directive.ts
@@ -6,7 +6,6 @@ import { Directive, HostBinding, Input } from "@angular/core";
 export class TableHeaderDescription {
 	static counter = 0;
 
-	@Input() id = `table-description-${TableHeaderDescription.counter++}`;
+	@HostBinding("attr.id") @Input() id = `table-description-${TableHeaderDescription.counter++}`;
 	@HostBinding("class.bx--data-table-header__description") descriptionClass = true;
-	@HostBinding("attr.id") setId = this.id;
 }
--- src/table/header/table-header-title.directive.ts
+++ src/table/header/table-header-title.directive.ts
@@ -6,7 +6,6 @@ import { Directive, HostBinding, Input } from "@angular/core";
 export class TableHeaderTitle {
 	static counter = 0;
 
-	@Input() id = `table-title-${TableHeaderTitle.counter++}`;
+	@HostBinding("attr.id") @Input() id = `table-title-${TableHeaderTitle.counter++}`;
 	@HostBinding("class.bx--data-table-header__title") titleClass = true;
-	@HostBinding("attr.id") setId = this.id;
 }
