6a9c1cabc33a5b9c8ffc0ad096e50f6729e84402
Moiz Masud <moizkhan_007@hotmail.com>
2023-02-15 04:04:05 -0500

    Update src/tiles/selection-tile.component.ts
    Co-authored-by: Akshat Patel <38994122+Akshat55@users.noreply.github.com>

--- src/tiles/selection-tile.component.ts
+++ src/tiles/selection-tile.component.ts
@@ -32,7 +32,7 @@ import { I18n } from "carbon-components-angular/i18n";
 				[type]="(multiple ? 'checkbox': 'radio')"
 				[value]="value"
 				[name]="name"
-				(change)="!disabled ? onChange($event) : null"/>
+				(change)="disabled ? null : onChange($event)"/>
 			<div class="bx--tile__checkmark">
 				<svg width="16" height="16" viewBox="0 0 16 16">
 					<path d="M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16zm3.646-10.854L6.75 10.043 4.354 7.646l-.708.708 3.104 3.103 5.604-5.603-.708-.708z"
