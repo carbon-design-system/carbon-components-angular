25c307cfe9298d7755d6b928a6819cf4a75f671b
Moiz Masud <moizkhan_007@hotmail.com>
2023-02-16 11:04:07 -0500

    Removed disable check from click event
    Signed-off-by: Moiz Masud <moizkhan_007@hotmail.com>

--- src/tiles/clickable-tile.component.ts
+++ src/tiles/clickable-tile.component.ts
@@ -29,7 +29,7 @@ import { Router } from "@angular/router";
 			'bx--tile--disabled bx--link--disabled' : disabled
 		}"
 		tabindex="0"
-		(click)="disabled ? null : navigate($event)"
+		(click)="navigate($event)"
 		[attr.href]="disabled ? null : href"
 		[attr.target]="target"
 		[attr.aria-disabled]="disabled">
@@ -74,7 +74,7 @@ export class ClickableTile {
 	constructor(@Optional() protected router: Router) {}
 
 	navigate(event) {
-		if (this.router && this.route) {
+		if (this.router && this.route && !this.disabled) {
 			event.preventDefault();
 			const status = this.router.navigate(this.route, this.routeExtras);
 			this.navigation.emit(status);
