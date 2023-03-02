8b6497f91c9cc72e455a7b7242d80201d35c70d3
rhythm.rhythm@ibm.com <rhythm.rhythm@ibm.com>
2023-03-02 17:28:56 +0530

    fix: dropdown list scroll with keyboard

--- src/dropdown/list/dropdown-list.component.ts
+++ src/dropdown/list/dropdown-list.component.ts
@@ -539,13 +539,13 @@ export class DropdownList implements AbstractDropdownView, AfterViewInit, OnDest
 			event.preventDefault();
 			if (event.key === "ArrowDown" || event.key === "Down") {
 				if (this.hasNextElement()) {
-					this.getNextElement();
+					this.getNextElement().scrollIntoView({behavior: 'smooth',block: 'end'})
 				} else {
 					this.blurIntent.emit("bottom");
 				}
 			} else if (event.key === "ArrowUp" || event.key === "Up") {
 				if (this.hasPrevElement()) {
-					this.getPrevElement();
+					this.getPrevElement().scrollIntoView({behavior: 'smooth'})
 				} else {
 					this.blurIntent.emit("top");
 				}
