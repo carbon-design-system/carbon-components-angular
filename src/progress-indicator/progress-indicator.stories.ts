9082ba955b08d0836086a8f0f4390b0e4a49ae9c
Rajeshkumar Umretiya <raj.umretiya@ibm.com>
2023-04-08 08:21:40 -0400

    fix: updated story data(steps) for better visulation of vertical indicator

--- src/progress-indicator/progress-indicator.stories.ts
+++ src/progress-indicator/progress-indicator.stories.ts
@@ -120,15 +120,25 @@ storiesOf("Components|Progress Indicator", module)
 			steps : [
 				{
 					text: "First step",
-					state: ["complete"]
+					state: ["complete"],
+					optionalText: "optional"
 				},
 				{
 					text: "Second step",
-					state: ["current"]
+					state: ["current"],
+					tooltip: { content: "Overflow tooltip content.", trigger: "click", placement: "bottom" }
 				},
 				{
 					text: "Third step",
-					state: ["incomplete"]
+					state: ["incomplete"],
+					tooltip: {
+						content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+							Animi consequuntur hic ratione aliquid cupiditate, nesciunt saepe iste
+							blanditiis cumque maxime tenetur veniam est illo deserunt sint quae pariatur.
+							Laboriosam, consequatur.`,
+						trigger: "click",
+						placement: "bottom"
+					}
 				},
 				{
 					text: "Fourth step",
