d8d5d55d379e5e6b221ca8908bc9c0f6a5be71b2
Eduard Marcinco <edomarcinco@gmail.com>
2024-08-30 08:55:39 +0200

    feat: readonly state for password input

--- src/input/password-input-label.component.ts
+++ src/input/password-input-label.component.ts
@@ -208,6 +208,9 @@ export class PasswordInputLabelComponent extends BaseIconButton implements After
 	@HostBinding("class.cds--form-item") labelClass = true;
 	@HostBinding("class.cds--password-input-wrapper") passwordInputWrapper = true;
 	@HostBinding("class.cds--text-input-wrapper") textInputWrapper = true;
+	@HostBinding("class.cds--text-input-wrapper--readonly") get isReadonly() {
+		return this.wrapper?.nativeElement.querySelector("input")?.readOnly;
+	}
 
 	/**
 	 * Constructor for PasswordInputLabelComponent.
