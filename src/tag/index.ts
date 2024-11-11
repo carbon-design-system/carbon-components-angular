6543175c6718430278d4648f800f7600f75b3a8b
Akshat Patel <akshat@live.ca>
2024-11-10 21:52:26 -0500

    feat: create clickable/selectable tag components
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/tag/index.ts
+++ src/tag/index.ts
@@ -1,3 +1,5 @@
 export * from "./tag-filter.component";
 export * from "./tag.component";
+export * from "./tag-icon.directive";
+export * from "./tag-selectable.component";
 export * from "./tag.module";
