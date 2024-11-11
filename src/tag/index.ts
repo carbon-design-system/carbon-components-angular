b30eb0d12542134922dba9edcf6026f4dfa42069
Akshat Patel <akshat@live.ca>
2024-11-10 22:13:42 -0500

    fix: export operational tag from directory
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/tag/index.ts
+++ src/tag/index.ts
@@ -2,4 +2,5 @@ export * from "./tag-filter.component";
 export * from "./tag.component";
 export * from "./tag-icon.directive";
 export * from "./tag-selectable.component";
+export * from "./tag-operational.component";
 export * from "./tag.module";
