a956e9d21e127ecfe5f6d32c1cd3e6cdc3e815ea
rrothschild18 <raultonello18@gmail.com>
2025-01-23 18:28:56 -0300

    feat: on push strategy for loader, part of #3066
    Signed-off-by: rrothschild18 <raultonello18@gmail.com>

--- src/loading/loading.component.ts
+++ src/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, HostBinding } from "@angular/core";
+import { Component, Input, HostBinding, ChangeDetectionStrategy } from "@angular/core";
 import { I18n } from "carbon-components-angular/i18n";
 
 /**
@@ -25,7 +25,8 @@ import { I18n } from "carbon-components-angular/i18n";
 				<circle class="cds--loading__stroke" cx="50%" cy="50%" r="44" />
 			</svg>
 		</div>
-	`
+	`,
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Loading {
 	/**
