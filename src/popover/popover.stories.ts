07a919367d61cc3172dec40e5723893a662ce812
Klaas Cuvelier <cuvelierklaas@gmail.com>
2025-02-26 14:29:33 +0100

    Update src/popover/popover.stories.ts
    Co-authored-by: Akshat Patel <38994122+Akshat55@users.noreply.github.com>

--- src/popover/popover.stories.ts
+++ src/popover/popover.stories.ts
@@ -143,8 +143,7 @@ const AutoAlignTemplate = (args) => ({
 							xmlns="http://www.w3.org/2000/svg"
 							width="16"
 							height="16"
-							viewBox="0 0 32 32"
-						>
+							viewBox="0 0 32 32">
 							<path d="M26,4H6A2,2,0,0,0,4,6V26a2,2,0,0,0,2,2H26a2,2,0,0,0,2-2V6A2,2,0,0,0,26,4ZM6,26V6H26V26Z"></path>
 						</svg>
 					</div>
