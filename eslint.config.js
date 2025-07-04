beca96aa5e805e18038db6e4e9369f49847984fc
Tom Wegener <tom.wegener@allegro-packets.com>
2025-07-04 12:32:57 +0200

    feat: update storybook to v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
