f26e14f848fc8db39def52b7f477798575f3c882
Akshat Patel <38994122+Akshat55@users.noreply.github.com>
2026-04-14 12:42:16 -0400

    refactor: rename directory to be singular
    Signed-off-by: Akshat Patel <38994122+Akshat55@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ export * from "carbon-components-angular/search";
 export * from "carbon-components-angular/select";
 export * from "carbon-components-angular/skeleton";
 export * from "carbon-components-angular/slider";
-export * from "carbon-components-angular/ai-labels";
+export * from "carbon-components-angular/ai-label";
 export * from "carbon-components-angular/structured-list";
 export * from "carbon-components-angular/table";
 export * from "carbon-components-angular/tabs";
