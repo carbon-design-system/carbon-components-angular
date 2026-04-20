c3b10b4e11820536788ce6eac82dd5853d9bbfae
Akshat Patel <38994122+Akshat55@users.noreply.github.com>
2026-04-20 16:15:34 -0400

    chore: ignore dist, node_modules and coverage directories from linting
    Signed-off-by: Akshat Patel <38994122+Akshat55@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,12 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import angular from "angular-eslint";
 
-export default tseslint.config({
-	files: ["src/**/*.ts"],
+export default tseslint.config(
+	{
+		ignores: ["**/dist/**", "**/node_modules/**", "**/coverage/**"]
+	},
+	{
+		files: ["src/**/*.ts"],
 	extends: [
 		eslint.configs.recommended,
 		...tseslint.configs.recommended,
