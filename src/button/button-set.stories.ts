cc8675ebc64858ef0722a6e2abfb0a347eada6e2
Akshat Patel <38994122+Akshat55@users.noreply.github.com>
2026-04-23 18:10:00 -0400

    feat: add stacked & fluid support
    Signed-off-by: Akshat Patel <38994122+Akshat55@users.noreply.github.com>

--- src/button/button-set.stories.ts
+++ src/button/button-set.stories.ts
@@ -8,25 +8,42 @@ export default {
 	title: "Components/Button/Button Set",
 	decorators: [
 		moduleMetadata({
-			imports: [
-				ButtonModule,
-				IconModule
-			]
-		})
+			imports: [ButtonModule, IconModule],
+		}),
 	],
 	parameters: {
-		layout: "centered"
+		layout: "centered",
 	},
-	component: ButtonSet
+	component: ButtonSet,
 } as Meta;
 
 const Template = (args) => ({
 	props: args,
 	template: `
 		<cds-button-set>
-			<button cdsButton="secondary" [size]="size" [isExpressive]="isExpressive">Button</button>
-			<button cdsButton [size]="size" [isExpressive]="isExpressive">Button</button>
+			<button cdsButton="secondary">Button</button>
+			<button cdsButton>Button</button>
 		</cds-button-set>
-	`
+	`,
 });
 export const Basic = Template.bind({});
+Basic.args = {
+	fluid: false,
+	stacked: false,
+};
+
+const FluidTemplate = (args) => ({
+	props: args,
+	template: `
+		<cds-button-set [fluid]="fluid" [stacked]="stacked" style="width: 800px;">
+			<button cdsButton="ghost" [size]="size">Cancel</button>
+			<button cdsButton="secondary" [size]="size">Button</button>
+			<button cdsButton>Button</button>
+		</cds-button-set>
+	`,
+});
+export const Fluid = FluidTemplate.bind({});
+Fluid.args = {
+	fluid: true,
+	stacked: false,
+};
